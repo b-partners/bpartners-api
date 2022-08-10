1a53df89a6338676ec3ebbf266108c15150b2a26
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-10 14:38:52 +0300

    feat:preregistration get and post mapping

--- src/main/java/app/bpartners/api/endpoint/rest/controller/PreRegistrationController.java
+++ src/main/java/app/bpartners/api/endpoint/rest/controller/PreRegistrationController.java
@@ -3,12 +3,16 @@ package app.bpartners.api.endpoint.rest.controller;
 
 import app.bpartners.api.endpoint.rest.mapper.PreRegistrationMapper;
 import app.bpartners.api.endpoint.rest.model.CreatePreRegistration;
-import app.bpartners.api.model.exception.NotImplementedException;
+import app.bpartners.api.endpoint.rest.model.PreRegistration;
 import app.bpartners.api.service.PreRegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -18,8 +22,16 @@ public class PreRegistrationController {
 
   private final PreRegistrationMapper preRegistrationMapper;
 
+  @GetMapping("/pre-registration")
+  public List<PreRegistration> getAllPreRegistration() {
+    return preRegistrationService.getPreRegistration()
+            .stream().map(PreRegistrationMapper::toRestPreRegistration)
+            .collect(Collectors.toUnmodifiableList());
+  }
+
   @PostMapping("/pre-registration")
-  public String createEmail(@RequestBody CreatePreRegistration toCreate) {
-    throw new NotImplementedException("Not implemented");
+  public PreRegistration createPregistration(@RequestBody CreatePreRegistration toCreate) {
+    app.bpartners.api.model.PreRegistration preRegistration = preRegistrationService.createPreRegistration(preRegistrationMapper.toDomain(toCreate));
+    return PreRegistrationMapper.toRestPreRegistration(preRegistration);
   }
 }

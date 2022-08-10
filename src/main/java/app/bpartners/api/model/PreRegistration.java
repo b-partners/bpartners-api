0257b555f2b55da17963115ec81c84b122955e49
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-10 22:30:10 +0300

    squash! feat:preregistration controller

--- src/main/java/app/bpartners/api/model/PreRegistration.java
+++ src/main/java/app/bpartners/api/model/PreRegistration.java
@@ -7,10 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.*;
 import org.hibernate.Hibernate;
 
 import static javax.persistence.GenerationType.IDENTITY;
@@ -19,6 +17,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 @AllArgsConstructor
 @NoArgsConstructor
 @Setter
+@Builder
 @Entity
 public class PreRegistration implements Serializable {
   @Id
--- src/main/java/app/bpartners/api/repository/EmailRepository.java
+++
@@ -1,9 +0,0 @@
-package app.bpartners.api.repository;
-
-import app.bpartners.api.model.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmailRepository extends JpaRepository<Email, String> {
-}

2e93e56088ad3cc6033fd9abc21e61eaa4f7f626
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-10 22:13:35 +0300

    feat:preregistration mapper

--- src/main/java/app/bpartners/api/endpoint/rest/mapper/PreRegistrationMapper.java
+++ src/main/java/app/bpartners/api/endpoint/rest/mapper/PreRegistrationMapper.java
@@ -1,11 +1,42 @@
 package app.bpartners.api.endpoint.rest.mapper;
 
+import app.bpartners.api.endpoint.rest.model.CreatePreRegistration;
+import app.bpartners.api.endpoint.rest.model.PreRegistration;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PreRegistrationMapper {
-  //TODO : Internal PreRegistration to REST PreRegistration
+  public PreRegistration toRestPreRegistration(app.bpartners.api.model.PreRegistration preRegistration) {
+    PreRegistration restPreRegistration = new PreRegistration();
+    restPreRegistration.setId(preRegistration.getId());
+    restPreRegistration.setFirstName(preRegistration.getFirstName());
+    restPreRegistration.setLastName(preRegistration.getLastName());
+    restPreRegistration.setEmail(preRegistration.getEmail());
+    restPreRegistration.setSociety(preRegistration.getSocietyName());
+    restPreRegistration.setEntranceDatetime(preRegistration.getEntranceDatetime());
+    restPreRegistration.setPhoneNumber(preRegistration.getPhoneNumber());
+    return restPreRegistration;
+  }
 
-  //TODO : REST PreRegistration to Internal PreRegistration
+  public app.bpartners.api.model.PreRegistration toDomain(CreatePreRegistration toCreate) {
+    app.bpartners.api.model.PreRegistration domainPreRegistration = new app.bpartners.api.model.PreRegistration();
+    domainPreRegistration.setFirstName(toCreate.getFirstName());
+    domainPreRegistration.setLastName(toCreate.getLastName());
+    domainPreRegistration.setEmail(toCreate.getEmail());
+    domainPreRegistration.setSocietyName(toCreate.getSociety());
+    domainPreRegistration.setPhoneNumber(toCreate.getPhoneNumber());
+    return domainPreRegistration;
+  }
 
+  public app.bpartners.api.model.PreRegistration toDomain(PreRegistration restPreRegistration) {
+    app.bpartners.api.model.PreRegistration domainPreRegistration = new app.bpartners.api.model.PreRegistration();
+    domainPreRegistration.setId(restPreRegistration.getId());
+    domainPreRegistration.setFirstName(restPreRegistration.getFirstName());
+    domainPreRegistration.setLastName(restPreRegistration.getLastName());
+    domainPreRegistration.setEmail(restPreRegistration.getEmail());
+    domainPreRegistration.setSocietyName(restPreRegistration.getSociety());
+    domainPreRegistration.setEntranceDatetime(restPreRegistration.getEntranceDatetime());
+    domainPreRegistration.setPhoneNumber(restPreRegistration.getPhoneNumber());
+    return domainPreRegistration;
+  }
 }

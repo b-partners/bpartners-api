09da80a12c86acf638ff4b2ae047daeb4e2a374e
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-10 22:14:18 +0300

    feat:preregistration controller

--- src/main/java/app/bpartners/api/endpoint/rest/controller/PreRegistrationController.java
+++ src/main/java/app/bpartners/api/endpoint/rest/controller/PreRegistrationController.java
@@ -3,7 +3,9 @@ package app.bpartners.api.endpoint.rest.controller;
 
 import app.bpartners.api.endpoint.rest.mapper.PreRegistrationMapper;
 import app.bpartners.api.endpoint.rest.model.CreatePreRegistration;
-import app.bpartners.api.model.exception.NotImplementedException;
+import app.bpartners.api.endpoint.rest.model.PreRegistration;
+import app.bpartners.api.model.exception.BadRequestException;
+import app.bpartners.api.model.validator.PreRegistrationValidator;
 import app.bpartners.api.service.PreRegistrationService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,8 +20,18 @@ public class PreRegistrationController {
 
   private final PreRegistrationMapper preRegistrationMapper;
 
+  private final PreRegistrationValidator validator;
+
   @PostMapping("/pre-registration")
-  public String createEmail(@RequestBody CreatePreRegistration toCreate) {
-    throw new NotImplementedException("Not implemented");
+  public PreRegistration createPreRegistration(@RequestBody CreatePreRegistration toCreate) {
+    if (validator.accept(toCreate)) {
+      app.bpartners.api.model.PreRegistration createdPreRegistration = preRegistrationMapper
+              .toDomain(toCreate);
+      return preRegistrationMapper
+              .toRestPreRegistration(preRegistrationService
+                      .createPreRegistration(createdPreRegistration));
+    } else {
+      throw new BadRequestException("Invalid email format");
+    }
   }
 }

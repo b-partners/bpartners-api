83352476bf90837abd1d166e0bf25c3be62dbfd5
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-08-18 17:22:06 +0300

    feat: userMapper
    HUser to Domain and Domain to HUser

--- src/main/java/app/bpartners/api/model/mapper/UserMapper.java
+++ src/main/java/app/bpartners/api/model/mapper/UserMapper.java
@@ -19,4 +19,30 @@ public class UserMapper {
         .status(hUser.getStatus())
         .build();
   }
+
+  public HUser toHibernate(User user) {
+    return HUser.builder()
+        .id(user.getId())
+        .monthlySubscription(user.getMonthlySubscription())
+        .swanUserId(user.getSwanUser().getId())
+        .status(user.getStatus())
+        .phoneNumber(user.getSwanUser().getMobilePhoneNumber())
+        .build();
+  }
+
+  public app.bpartners.api.endpoint.rest.model.User toRest(User user){
+    app.bpartners.api.endpoint.rest.model.User restUser = new app.bpartners.api.endpoint.rest.model.User();
+    restUser.setId(user.getId());
+    restUser.setBirthDate(user.getSwanUser().getBirthDate());
+    restUser.setFirstName(user.getSwanUser().getFirstName());
+    restUser.setLastName(user.getSwanUser().getLastName());
+    restUser.setStatus(user.getStatus());
+    restUser.setIdentificationStatus(user.getSwanUser().getIdentificationStatus());
+    restUser.setMonthlySubscription(user.getMonthlySubscription());
+    restUser.setIdVerified(user.getSwanUser().getIdVerified());
+    restUser.setNationalityCCA3(user.getSwanUser().getNationalityCCA3());
+    restUser.setSwanId(user.getSwanUser().getId());
+    restUser.setMobilePhoneNumber(user.getSwanUser().getMobilePhoneNumber());
+    return restUser;
+  }
 }

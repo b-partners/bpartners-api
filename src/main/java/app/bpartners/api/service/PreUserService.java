12ff0cd79f3dac16d5f1b2b683a7659291b63a17
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-08-23 15:36:27 +0300

    chore: preUserValidator

--- src/main/java/app/bpartners/api/service/PreUserService.java
+++ src/main/java/app/bpartners/api/service/PreUserService.java
@@ -4,6 +4,7 @@ package app.bpartners.api.service;
 import app.bpartners.api.model.PreUser;
 import app.bpartners.api.model.entity.BoundedPageSize;
 import app.bpartners.api.model.entity.PageFromOne;
+import app.bpartners.api.model.validator.PreUserValidator;
 import app.bpartners.api.repository.implementation.PreUserRepositoryImpl;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -15,8 +16,10 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class PreUserService {
   private final PreUserRepositoryImpl repository;
+  private final PreUserValidator preUserValidator;
 
   public List<PreUser> createPreUsers(List<PreUser> toCreate) {
+    preUserValidator.accept(toCreate);
     return repository.createPreUsers(toCreate);
   }
 

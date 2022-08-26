9f0c7721bffa32766a5511ce41cf06b466f47c77
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-08-26 17:26:06 +0300

    chore: reformat swanApi

--- src/main/java/app/bpartners/api/repository/swan/implementation/AccountHolderSwanRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/swan/implementation/AccountHolderSwanRepositoryImpl.java
@@ -1,58 +1,25 @@
 package app.bpartners.api.repository.swan.implementation;
 
-import app.bpartners.api.endpoint.rest.security.model.Principal;
-import app.bpartners.api.endpoint.rest.security.principal.PrincipalProvider;
-import app.bpartners.api.endpoint.rest.security.swan.SwanConf;
-import app.bpartners.api.model.exception.ApiException;
+import app.bpartners.api.repository.api.swan.SwanApi;
 import app.bpartners.api.repository.swan.AccountHolderSwanRepository;
 import app.bpartners.api.repository.swan.response.AccountHolderResponse;
-import app.bpartners.api.repository.swan.model.AccountHolder;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import app.bpartners.api.repository.swan.schema.AccountHolder;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import static app.bpartners.api.endpoint.rest.security.swan.SwanConf.BEARER_PREFIX;
-
 @Repository
 @AllArgsConstructor
 public class AccountHolderSwanRepositoryImpl implements AccountHolderSwanRepository {
+  private static String message =
+      "{ \"query\": \"" + "query AccountHolder { accountHolders { edges { node "
+          + "{ id info { name } residencyAddress "
+          + "{ addressLine1 city country postalCode } } } }}\"}";
 
-  private final PrincipalProvider auth;
-
-  private SwanConf swanConf;
+  private final SwanApi<AccountHolderResponse> swanApi;
 
   @Override
   public List<AccountHolder> getAccountHolders() {
-    try {
-      HttpClient httpClient = HttpClient.newBuilder().build();
-      String message = "{ \"query\": \"" + "query AccountHolder { accountHolders { edges { node "
-          + "{ id info { name } residencyAddress "
-          + "{ addressLine1 city country postalCode } } } }}\"}";
-      HttpRequest request = HttpRequest.newBuilder().uri(new URI(swanConf.getApiUrl()))
-          .header("Content-Type", "application/json")
-          .header("Authorization", BEARER_PREFIX + bearerToken())
-          .POST(HttpRequest.BodyPublishers.ofString(message)).build();
-      HttpResponse<String> response =
-          httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-      AccountHolderResponse accountHolderResponse =
-          new ObjectMapper().findAndRegisterModules() //Load DateTime Module
-              .readValue(response.body(), AccountHolderResponse.class);
-      AccountHolder accountHolder =
-          accountHolderResponse.data.accountHolders.edges.get(0).node;
-      return List.of(accountHolder);
-    } catch (IOException | InterruptedException | URISyntaxException e) {
-      throw new ApiException(ApiException.ExceptionType.SERVER_EXCEPTION, e);
-    }
-  }
-
-  private String bearerToken() {
-    return ((Principal) auth.getAuthentication().getPrincipal()).getBearer();
+    return List.of(swanApi.getData(message, "").data.accountHolders.edges.get(0).node);
   }
 }
--- src/main/java/app/bpartners/api/repository/swan/implementation/AccountSwanRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/swan/implementation/AccountSwanRepositoryImpl.java
@@ -1,56 +1,23 @@
 package app.bpartners.api.repository.swan.implementation;
 
-import app.bpartners.api.endpoint.rest.security.model.Principal;
-import app.bpartners.api.endpoint.rest.security.principal.PrincipalProvider;
-import app.bpartners.api.endpoint.rest.security.swan.SwanConf;
-import app.bpartners.api.model.exception.ApiException;
+import app.bpartners.api.repository.api.swan.SwanApi;
 import app.bpartners.api.repository.swan.AccountSwanRepository;
 import app.bpartners.api.repository.swan.response.AccountResponse;
-import app.bpartners.api.repository.swan.model.SwanAccount;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import app.bpartners.api.repository.swan.schema.SwanAccount;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import static app.bpartners.api.endpoint.rest.security.swan.SwanConf.BEARER_PREFIX;
-
 @Repository
 @AllArgsConstructor
 public class AccountSwanRepositoryImpl implements AccountSwanRepository {
-  private final PrincipalProvider auth;
-  private SwanConf swanConf;
+  private static String message =
+      "{\"query\": \"query Account { accounts { edges { node { id name IBAN BIC } } } } \"}";
+  private final SwanApi<AccountResponse> swanApi;
 
   @Override
   public List<SwanAccount> getAccounts() {
-    try {
-      HttpClient httpClient = HttpClient.newBuilder().build();
-      String message =
-          "{\"query\": \"query Account { accounts { edges { node { id name IBAN BIC } } } } \"}";
-      HttpRequest request = HttpRequest.newBuilder()
-          .uri(new URI(swanConf.getApiUrl()))
-          .header("Content-Type", "application/json")
-          .header("Authorization", BEARER_PREFIX + bearerToken())
-          .POST(HttpRequest.BodyPublishers.ofString(message))
-          .build();
-      HttpResponse<String> response =
-          httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-      AccountResponse accountResponse = new ObjectMapper()
-          .findAndRegisterModules()
-          .readValue(response.body(), AccountResponse.class);
-      SwanAccount account = accountResponse.data.accounts.edges.get(0).node;
-      return List.of(account);
-    } catch (IOException | InterruptedException | URISyntaxException e) {
-      throw new ApiException(ApiException.ExceptionType.SERVER_EXCEPTION, e);
-    }
-  }
-
-  private String bearerToken() {
-    return ((Principal) auth.getAuthentication().getPrincipal()).getBearer();
+    SwanAccount account = swanApi.getData(message, "").data.accounts.edges.get(0).node;
+    return List.of(account);
   }
 }

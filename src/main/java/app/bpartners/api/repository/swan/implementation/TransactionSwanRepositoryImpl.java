0e645a6a609def2594ea9467206f95fe0247bde6
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-09-07 13:09:24 +0300

    chore: reformat swanApi

--- src/main/java/app/bpartners/api/repository/swan/implementation/TransactionSwanRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/swan/implementation/TransactionSwanRepositoryImpl.java
@@ -1,56 +1,27 @@
 package app.bpartners.api.repository.swan.implementation;
 
-import app.bpartners.api.endpoint.rest.security.model.Principal;
-import app.bpartners.api.endpoint.rest.security.principal.PrincipalProvider;
-import app.bpartners.api.endpoint.rest.security.swan.SwanConf;
-import app.bpartners.api.model.exception.ApiException;
+import app.bpartners.api.repository.swan.SwanApi;
 import app.bpartners.api.repository.swan.TransactionSwanRepository;
-import app.bpartners.api.repository.swan.response.TransactionResponse;
 import app.bpartners.api.repository.swan.model.Transaction;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import app.bpartners.api.repository.swan.response.TransactionResponse;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import static app.bpartners.api.endpoint.rest.security.swan.SwanConf.BEARER_PREFIX;
-
 @Repository
 @AllArgsConstructor
 public class TransactionSwanRepositoryImpl implements TransactionSwanRepository {
-  private final PrincipalProvider auth;
-  private SwanConf swanConf;
+
+  private static final String query =
+      "{\"query\": \"query Transactions { accounts { edges { node { transactions { edges { "
+          + "node { id label reference amount { value currency } createdAt } } } } } }}\"}";
+
+  private final SwanApi<TransactionResponse> swanApi;
 
   @Override
   public List<Transaction> getTransactions() {
-    try {
-      HttpClient httpClient = HttpClient.newBuilder().build();
-      String message =
-          "{\"query\": \"query Transactions { accounts { edges { node { transactions { edges { "
-              + "node { id label reference amount { value currency } createdAt } } } } } }}\"}";
-      HttpRequest request = HttpRequest.newBuilder()
-          .uri(new URI(swanConf.getApiUrl()))
-          .header("Content-Type", "application/json")
-          .header("Authorization", BEARER_PREFIX + bearerToken())
-          .POST(HttpRequest.BodyPublishers.ofString(message))
-          .build();
-      HttpResponse<String> response =
-          httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-      TransactionResponse transactionResponse = new ObjectMapper()
-          .findAndRegisterModules() //Load DateTime Module
-          .readValue(response.body(), TransactionResponse.class);
-      return transactionResponse.data.accounts.edges.get(0).node.transactions.edges;
-    } catch (IOException | InterruptedException | URISyntaxException e) {
-      throw new ApiException(ApiException.ExceptionType.SERVER_EXCEPTION, e);
-    }
+    return swanApi.getData(TransactionResponse.class, query).getData().getAccounts().getEdges().get(
+        0).getNode().getTransactions().getEdges();
   }
 
-  private String bearerToken() {
-    return ((Principal) auth.getAuthentication().getPrincipal()).getBearer();
-  }
 }

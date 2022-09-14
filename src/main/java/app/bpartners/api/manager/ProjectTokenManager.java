55af50c49aafbfcce24959378c0e672e529b4bbf
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-09-14 16:41:05 +0300

    chore: refresh swan project token every hour

--- src/main/java/app/bpartners/api/manager/ProjectTokenManager.java
+++ src/main/java/app/bpartners/api/manager/ProjectTokenManager.java
@@ -1,27 +1,42 @@
 package app.bpartners.api.manager;
 
 import app.bpartners.api.model.exception.ApiException;
+import app.bpartners.api.repository.swan.SwanApi;
+import app.bpartners.api.repository.swan.response.ProjectTokenResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
 import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
+import software.amazon.awssdk.services.ssm.model.PutParameterRequest;
 import software.amazon.awssdk.services.ssm.model.SsmException;
 
 @Component
+@EnableScheduling
 public class ProjectTokenManager {
-  private SsmClient ssmClient;
+  private static final String SSM_STRING_PARAMETER_TYPE = "String";
   private final String swanProjectParamName;
   private final String fintectureProjectParamName;
+  private final SwanApi<ProjectTokenResponse> swanApi;
+  private SsmClient ssmClient;
+  @Value("${swan.client.id}")
+  private String clientId;
+  @Value("${swan.client.secret}")
+  private String clientSecret;
 
   public ProjectTokenManager(SsmClient ssmClient,
                              @Value("${aws.ssm.swan.project.param}")
                              String swanProjectParamName,
                              @Value("${fintecture.project.param.name}")
-                             String fintectureProjectParamName) {
+                             String fintectureProjectParamName,
+                             SwanApi<ProjectTokenResponse> swanApi
+  ) {
     this.ssmClient = ssmClient;
     this.swanProjectParamName = swanProjectParamName;
     this.fintectureProjectParamName = fintectureProjectParamName;
+    this.swanApi = swanApi;
   }
 
   private String getParameterValue(SsmClient ssmClient, String parameterName) {
@@ -44,5 +59,19 @@ public class ProjectTokenManager {
 
   public String getFintectureProjectToken() {
     return getParameterValue(ssmClient, fintectureProjectParamName);
+  }
+
+  @Scheduled(cron = "0 * * * * ?")
+  public void refreshSwanProjectToken() {
+    String accessToken =
+        swanApi.getProjectToken().getAccessToken();
+    ssmClient.putParameter(PutParameterRequest
+        .builder()
+        .value(accessToken)
+        .name(swanProjectParamName)
+        .type(SSM_STRING_PARAMETER_TYPE)
+        .overwrite(true)
+        .build()
+    );
   }
 }
--- src/main/java/app/bpartners/api/repository/swan/SwanApi.java
+++ src/main/java/app/bpartners/api/repository/swan/SwanApi.java
@@ -4,16 +4,22 @@ import app.bpartners.api.endpoint.rest.security.model.Principal;
 import app.bpartners.api.endpoint.rest.security.principal.PrincipalProvider;
 import app.bpartners.api.endpoint.rest.security.swan.SwanConf;
 import app.bpartners.api.model.exception.ApiException;
+import app.bpartners.api.repository.swan.response.ProjectTokenResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import static app.bpartners.api.endpoint.rest.security.swan.SwanConf.BEARER_PREFIX;
+import static app.bpartners.api.endpoint.rest.security.swan.SwanConf.SWAN_TOKEN_URL;
 
 /*
  *
@@ -47,5 +53,31 @@ public class SwanApi<T> {
 
   private String bearerToken() {
     return ((Principal) auth.getAuthentication().getPrincipal()).getBearer();
+  }
+
+  public ProjectTokenResponse getProjectToken() {
+    try {
+      HttpClient httpClient = HttpClient.newBuilder().build();
+      HttpRequest request = HttpRequest.newBuilder().uri(new URI(SWAN_TOKEN_URL))
+          .header("Content-Type", "application/x-www-form-urlencoded")
+          .POST(getParamsUrlEncoded(swanConf.getParams())).build();
+      HttpResponse<String> response =
+          httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+      return new ObjectMapper().findAndRegisterModules()//Load DateTime Module
+          .readValue(response.body(), ProjectTokenResponse.class);
+    } catch (InterruptedException | IOException | URISyntaxException e) {
+      throw new ApiException(ApiException.ExceptionType.SERVER_EXCEPTION, e);
+    }
+  }
+  /*
+    TODO: if getProjectToken fails you should get a new Token
+   */
+
+  private HttpRequest.BodyPublisher getParamsUrlEncoded(Map<String, String> parameters) {
+    String urlEncoded = parameters.entrySet()
+        .stream()
+        .map(e -> e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8))
+        .collect(Collectors.joining("&"));
+    return HttpRequest.BodyPublishers.ofString(urlEncoded);
   }
 }

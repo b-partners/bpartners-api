b85885c7586a7812253a810a3275b1c25efb5ce9
Ryan Andriamahery <ryan@hei.school>
2022-09-16 17:35:48 +0300

    chore: user access token provider log error

--- src/main/java/app/bpartners/api/endpoint/rest/security/swan/SwanComponent.java
+++ src/main/java/app/bpartners/api/endpoint/rest/security/swan/SwanComponent.java
@@ -15,6 +15,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.http.Header;
 
@@ -22,6 +23,7 @@ import static app.bpartners.api.endpoint.rest.security.swan.SwanConf.BEARER_PREF
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class SwanComponent {
   private final SwanMapper swanMapper;
   private final SwanConf swanConf;
@@ -57,6 +59,7 @@ public class SwanComponent {
           TokenResponse.class);
       return swanMapper.toRest(tokenResponse);
     } catch (IOException | InterruptedException | URISyntaxException e) {
+      log.info("Error", e);
       throw new BadRequestException("Code is invalid, expired, revoked or the redirectUrl "
           + redirectUrl + " does not match in the authorization request");
     }
--- src/main/java/app/bpartners/api/repository/swan/response/TokenResponse.java
+++ src/main/java/app/bpartners/api/repository/swan/response/TokenResponse.java
@@ -2,16 +2,16 @@ package app.bpartners.api.repository.swan.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class TokenResponse extends ProjectTokenResponse {
   private static final String JSON_PROPERTY_ID_TOKEN = "id_token";
   private static final String JSON_PROPERTY_REFRESH_TOKEN = "refresh_token";
   private String idToken;
-  private String  refreshToken;
-
-  public TokenResponse(String expiresIn, String scope, String tokenType, String accessToken) {
-    super(expiresIn, scope, tokenType, accessToken);
-  }
+  private String refreshToken;
 
   @JsonProperty(JSON_PROPERTY_ID_TOKEN)
   @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)

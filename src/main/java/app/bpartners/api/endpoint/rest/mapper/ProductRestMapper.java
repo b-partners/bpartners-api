f69772788b5b9432c763d01b50881a90f8bb38c0
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-09-23 16:23:38 +0300

    chore: rest product validator

--- src/main/java/app/bpartners/api/endpoint/rest/mapper/ProductRestMapper.java
+++ src/main/java/app/bpartners/api/endpoint/rest/mapper/ProductRestMapper.java
@@ -2,27 +2,33 @@ package app.bpartners.api.endpoint.rest.mapper;
 
 import app.bpartners.api.endpoint.rest.model.CreateProduct;
 import app.bpartners.api.endpoint.rest.model.Product;
+import app.bpartners.api.endpoint.rest.validator.CreateProductValidator;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class ProductRestMapper {
+  private final CreateProductValidator validator;
+
   public Product toRest(app.bpartners.api.model.Product domain) {
     return new Product()
-        .id(domain.getId())
-        .description(domain.getDescription())
-        .quantity(domain.getQuantity())
-        .unitPrice(domain.getUnitPrice())
-        .vatPercent(domain.getVatPercent())
-        .totalVat(domain.getTotalVat())
-        .totalPriceWithVat(domain.getTotalPriceWithVat());
+            .id(domain.getId())
+            .description(domain.getDescription())
+            .quantity(domain.getQuantity())
+            .unitPrice(domain.getUnitPrice())
+            .vatPercent(domain.getVatPercent())
+            .totalVat(domain.getTotalVat())
+            .totalPriceWithVat(domain.getTotalPriceWithVat());
   }
 
   public app.bpartners.api.model.Product toDomain(CreateProduct createProduct) {
+    validator.accept(createProduct);
     return app.bpartners.api.model.Product.builder()
-        .description(createProduct.getDescription())
-        .unitPrice(createProduct.getUnitPrice())
-        .quantity(createProduct.getQuantity())
-        .vatPercent(createProduct.getVatPercent())
-        .build();
+            .description(createProduct.getDescription())
+            .unitPrice(createProduct.getUnitPrice())
+            .quantity(createProduct.getQuantity())
+            .vatPercent(createProduct.getVatPercent())
+            .build();
   }
 }
---
+++ src/test/java/app/bpartners/api/unit/validator/CreateProductValidatorTest.java
@@ -0,0 +1,41 @@
+package app.bpartners.api.unit.validator;
+
+import app.bpartners.api.endpoint.rest.model.CreateProduct;
+import app.bpartners.api.endpoint.rest.validator.CreateProductValidator;
+import app.bpartners.api.model.exception.BadRequestException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CreateProductValidatorTest {
+  private final CreateProductValidator validator = new CreateProductValidator();
+
+  CreateProduct validProduct() {
+    return new CreateProduct()
+        .description("Valid product")
+        .unitPrice(25000)
+        .quantity(10)
+        .vatPercent(75);
+  }
+
+  CreateProduct invalidProduct() {
+    return new CreateProduct()
+        .description("Invalid product")
+        .unitPrice(null)
+        .quantity(null)
+        .vatPercent(null);
+  }
+
+  @Test
+  void valid_product() {
+    CreateProduct validProduct = validProduct();
+    assertDoesNotThrow(() -> validator.accept(validProduct));
+  }
+
+  @Test
+  void invalid_product() {
+    CreateProduct invalidProduct = invalidProduct();
+    assertThrows(BadRequestException.class, () -> validator.accept(invalidProduct));
+  }
+}

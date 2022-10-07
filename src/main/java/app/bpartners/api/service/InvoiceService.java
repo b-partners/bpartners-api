1f1479bef84554f8f9c3709d08ef447abfaff31c
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-10-07 09:09:26 +0300

    chore: redesign invoice template

--- src/main/java/app/bpartners/api/service/InvoiceService.java
+++ src/main/java/app/bpartners/api/service/InvoiceService.java
@@ -14,10 +14,13 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.BaseFont;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.FileSystems;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.Base64;
 import java.util.List;
 import javax.imageio.ImageIO;
@@ -108,23 +111,38 @@ public class InvoiceService {
 
   private String parseInvoiceTemplateToString(String idInvoice) {
     //Invoice invoice = getById(idInvoice);
+    Product product = Product.builder()
+        .description("Product description")
+        .quantity(50)
+        .vatPercent(5)
+        .unitPrice(15)
+        .build();
     Invoice invoice = Invoice.builder()
+        .toPayAt(LocalDate.now())
         .invoiceCustomer(InvoiceCustomer.customerTemplateBuilder()
             .name("Client")
             .address("Mon adresse, Paris, France")
+            .email("example@email.com")
+            .phone("+33 6 24 15 48 45")
+            .website("www.website.com")
             .build())
+        .products(List.of(product))
         .account(Account.builder()
             .id("beed1765-5c16-472a-b3f4-5c376ce5db58")
+            .name("Artisan")
+            .iban("FR7612548029989876543210917")
             .build())
         .paymentUrl("https://dashboard-dev.bpartners.app")
         .build();
     TemplateEngine templateEngine = configureTemplate();
+    Account account = invoice.getAccount();
     Context context = new Context();
     byte[] qrCodeBytes = generateQrCode(invoice.getPaymentUrl());
-    byte[] logoBytes = fileService.downloadFile(invoice.getAccount().getId(), LOGO_JPEG);
+    byte[] logoBytes = fileService.downloadFile(account.getId(), LOGO_JPEG);
     context.setVariable("invoice", invoice);
     context.setVariable("qrcode", base64Image(qrCodeBytes));
     context.setVariable("logo", base64Image(logoBytes));
+    context.setVariable("account", account);
 
     return templateEngine.process("invoice", context);
   }
@@ -145,14 +163,15 @@ public class InvoiceService {
 
   private void loadStyle(ITextRenderer renderer, String identifier) {
     try {
+      renderer.getFontResolver().addFont("templates/code39.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
       String baseUrl = FileSystems.getDefault()
           .getPath("src/main", "resources", "templates")
           .toUri()
           .toURL()
           .toString();
       renderer.setDocumentFromString(parseInvoiceTemplateToString(identifier), baseUrl);
-    } catch (MalformedURLException e) {
-      throw new RuntimeException(e);
+    } catch (DocumentException | IOException e) {
+      throw new ApiException(SERVER_EXCEPTION, e);
     }
   }
 

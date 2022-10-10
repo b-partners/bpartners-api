a1ac772499f91cd125e478256606fc84f7bae703
Ryan Andriamahery <ryan@hei.school>
2022-10-10 17:38:59 +0300

    chore: set file download test insted of logo

--- src/main/resources/db/testdata/V99_9__Test_create_file.sql
+++ src/main/resources/db/testdata/V99_9__Test_create_file.sql
@@ -1,4 +1,7 @@
 insert into "file_info"
 (id, uploaded_at, size_in_kb, sha256, account_id)
 values ('logo.jpeg', '2022-08-31T13:35:26.853Z', 76,
+        '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',
+        'beed1765-5c16-472a-b3f4-5c376ce5db58'),
+       ('test.jpeg', '2022-08-31T13:35:26.853Z', 76,
         '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824','beed1765-5c16-472a-b3f4-5c376ce5db58');
--- src/test/java/app/bpartners/api/integration/conf/TestUtils.java
+++ src/test/java/app/bpartners/api/integration/conf/TestUtils.java
@@ -43,7 +43,7 @@ public class TestUtils {
   public static final String INVOICE2_ID = "invoice2_id";
 
   public static final String INVOICE1_FILE_ID = "BP001.pdf";
-  public static final String FILE_ID = "logo.jpeg";
+  public static final String FILE_ID = "test.jpeg";
   public static final String TO_UPLOAD_FILE_ID = "to_upload_file_id.jpeg";
   public static final String MARKETPLACE1_ID = "marketplace1_id";
   public static final String MARKETPLACE2_ID = "marketplace2_id";

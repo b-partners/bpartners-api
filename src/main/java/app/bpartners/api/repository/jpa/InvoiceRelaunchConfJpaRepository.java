b088fd541f2cf2f934e06cfce44a388ab20b1707
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-10-28 08:06:07 +0300

    chore: rename invoice relaunch to invoice relaunch conf

--- src/main/java/app/bpartners/api/repository/jpa/InvoiceRelaunchConfJpaRepository.java
+++ src/main/java/app/bpartners/api/repository/jpa/InvoiceRelaunchConfJpaRepository.java
@@ -4,6 +4,6 @@ import app.bpartners.api.repository.jpa.model.HInvoiceRelaunchConf;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InvoiceRelaunchJpaRepository extends JpaRepository<HInvoiceRelaunchConf, String> {
+public interface InvoiceRelaunchConfJpaRepository extends JpaRepository<HInvoiceRelaunchConf, String> {
   Optional<HInvoiceRelaunchConf> getByAccountId(String accountId);
 }

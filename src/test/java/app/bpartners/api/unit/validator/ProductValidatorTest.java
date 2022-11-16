b64153493fcfecf3ef9072debf55219021f4ea0a
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-11-16 07:47:30 +0300

    chore: add product validator unit test

---
+++ src/test/java/app/bpartners/api/unit/validator/ProductValidatorTest.java
@@ -0,0 +1,40 @@
+package app.bpartners.api.unit.validator;
+
+import app.bpartners.api.endpoint.rest.model.Product;
+import app.bpartners.api.endpoint.rest.validator.ProductValidator;
+import org.junit.jupiter.api.Test;
+
+import static app.bpartners.api.integration.conf.TestUtils.assertThrowsBadRequestException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class ProductValidatorTest {
+  private final ProductValidator validator = new ProductValidator();
+
+  @Test
+  void valid_product() {
+    assertDoesNotThrow(() ->
+        validator.accept(
+            new Product()
+                .description("Product description")
+                .quantity(50)
+                .unitPrice(10.0)
+                .vatPercent(20.0)
+        ));
+  }
+
+  @Test
+  void invalid_product() {
+    assertThrowsBadRequestException(
+        "Description is mandatory. "
+            + "Unit price is mandatory. "
+            + "Quantity is mandatory. "
+            + "Vat percent is mandatory. ",
+        () -> validator.accept(
+            new Product()
+                .description(null)
+                .unitPrice(null)
+                .quantity(null)
+                .vatPercent(null)
+        ));
+  }
+}

a56ff0f7af5f1990e96d66adbc00b8177de1bf4e
Ryan Andriamahery <ryan@hei.school>
2022-11-17 17:29:44 +0300

    chore: remove redundant authenticated account check in transaction repository

--- src/main/java/app/bpartners/api/repository/implementation/TransactionRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/implementation/TransactionRepositoryImpl.java
@@ -1,10 +1,7 @@
 package app.bpartners.api.repository.implementation;
 
-import app.bpartners.api.model.Account;
 import app.bpartners.api.model.Transaction;
-import app.bpartners.api.model.exception.ForbiddenException;
 import app.bpartners.api.model.mapper.TransactionMapper;
-import app.bpartners.api.repository.AccountRepository;
 import app.bpartners.api.repository.TransactionCategoryRepository;
 import app.bpartners.api.repository.TransactionRepository;
 import app.bpartners.api.repository.swan.TransactionSwanRepository;
@@ -18,16 +15,10 @@ import org.springframework.stereotype.Repository;
 public class TransactionRepositoryImpl implements TransactionRepository {
   private final TransactionSwanRepository swanRepository;
   private final TransactionMapper mapper;
-  private final AccountRepository accountRepository;
   private final TransactionCategoryRepository categoryRepository;
 
   @Override
   public List<Transaction> findByAccountId(String id) {
-    Account authenticatedAccount = accountRepository.findAll().get(0);
-    if (!id.equals(authenticatedAccount.getId())) {
-      throw new ForbiddenException();
-    }
-    //TODO: replace this with persisted ID of TransactionCategory
     return swanRepository.getTransactions().stream()
         .map(transaction -> mapper.toDomain(transaction,
             categoryRepository.findByIdTransaction(transaction.getNode().getId())))

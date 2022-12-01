bd278d0c65dedde5b89ebd530125f3d8e7749051
Ryan Andriamahery <ryan@hei.school>
2022-12-01 20:25:57 +0300

    fix: return category template ID when category is null

--- src/main/java/app/bpartners/api/repository/TransactionCategoryTemplateRepository.java
+++ src/main/java/app/bpartners/api/repository/TransactionCategoryTemplateRepository.java
@@ -1,6 +1,5 @@
 package app.bpartners.api.repository;
 
-import app.bpartners.api.model.Fraction;
 import app.bpartners.api.model.TransactionCategoryTemplate;
 
 public interface TransactionCategoryTemplateRepository {

606202babb71fafe38fc40e1d608c4543ea7919d
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-12-09 11:54:10 +0300

    fix: add specific relaunch configuration for each invoice

--- src/main/java/app/bpartners/api/model/mapper/InvoiceRelaunchConfMapper.java
+++ src/main/java/app/bpartners/api/model/mapper/InvoiceRelaunchConfMapper.java
@@ -2,7 +2,9 @@ package app.bpartners.api.model.mapper;
 
 import app.bpartners.api.endpoint.rest.security.AuthenticatedResourceProvider;
 import app.bpartners.api.model.AccountInvoiceRelaunchConf;
+import app.bpartners.api.model.InvoiceRelaunchConf;
 import app.bpartners.api.repository.jpa.model.HAccountInvoiceRelaunchConf;
+import app.bpartners.api.repository.jpa.model.HInvoiceRelaunchConf;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -20,12 +22,32 @@ public class InvoiceRelaunchConfMapper {
         .build();
   }
 
+  public final InvoiceRelaunchConf toDomain(HInvoiceRelaunchConf entity) {
+    return InvoiceRelaunchConf
+        .builder()
+        .id(entity.getId())
+        .idInvoice(entity.getIdInvoice())
+        .delay(entity.getDelay())
+        .rehearsalNumber(entity.getRehearsalNumber())
+        .build();
+  }
+
   public HAccountInvoiceRelaunchConf toEntity(AccountInvoiceRelaunchConf domain) {
     return HAccountInvoiceRelaunchConf.builder()
         .id(domain.getId())
         .accountId(authResourceProvider.getAccount().getId())
         .draftRelaunch(domain.getDraftRelaunch())
         .unpaidRelaunch(domain.getUnpaidRelaunch())
+        .build();
+  }
+
+  public HInvoiceRelaunchConf toEntity(InvoiceRelaunchConf domain) {
+    return HInvoiceRelaunchConf
+        .builder()
+        .id(domain.getId())
+        .idInvoice(domain.getIdInvoice())
+        .delay(domain.getDelay())
+        .rehearsalNumber(domain.getRehearsalNumber())
         .build();
   }
 }

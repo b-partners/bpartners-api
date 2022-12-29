3d70dad57d155e92158ece8bb00199fd3d94d998
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-12-29 14:09:21 +0300

    fix: remove redundant mapper for transaction category vat
    closes #326

--- src/test/java/app/bpartners/api/integration/TransactionIT.java
+++ src/test/java/app/bpartners/api/integration/TransactionIT.java
@@ -22,7 +22,6 @@ import app.bpartners.api.repository.swan.TransactionSwanRepository;
 import app.bpartners.api.repository.swan.UserSwanRepository;
 import java.time.LocalDate;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -128,7 +127,6 @@ class TransactionIT {
     PayingApi api = new PayingApi(joeDoeClient);
 
     List<Transaction> actual = api.getTransactions(JOE_DOE_ACCOUNT_ID);
-
     assertEquals(3, actual.size());
     assertTrue(actual.contains(restTransaction1()));
     assertTrue(actual.contains(restTransaction2()));

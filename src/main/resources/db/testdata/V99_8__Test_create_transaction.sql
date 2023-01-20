f9f05da3cd4fd52f8b0cf14808a3b626098002c7
Mahefa Ny Anjara <98638690+Mahefaa@users.noreply.github.com>
2023-01-20 12:38:35 +0300

    chore: persist transactions

---
+++ src/main/resources/db/testdata/V99_8__Test_create_transaction.sql
@@ -0,0 +1,5 @@
+insert into transaction (id, id_swan, id_account)
+values ('transaction1_id', 'bosci_f224704f2555a42303e302ffb8e69eef',
+        'beed1765-5c16-472a-b3f4-5c376ce5db58'),
+       ('transaction2_id', 'bosci_0fe167566b234808a44aae415f057b6c',
+        'beed1765-5c16-472a-b3f4-5c376ce5db58');

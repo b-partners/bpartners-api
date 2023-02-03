6a198e633476ed299d8b03a24aaf761ccaded54b
Onitsiky <hei.onitsiky@gmail.com>
2023-02-03 11:22:24 +0300

    feat: add comment field to customer

--- src/main/resources/db/testdata/V99_3__Test_create_customer.sql
+++ src/main/resources/db/testdata/V99_3__Test_create_customer.sql
@@ -1,15 +1,15 @@
 insert into "customer"
-(id, id_account, "name", email, phone, website, address, zip_code, city, country)
+(id, id_account, "name", email, phone, website, address, zip_code, city, country, comment)
 values ('customer1_id', 'beed1765-5c16-472a-b3f4-5c376ce5db58', 'Luc Artisan', 'bpartners.artisans@gmail.com',
         '+33 12 34 56 78', 'https://luc.website.com', '15 rue Porte d''Orange', 95160,
-        'Montmorency', 'France'),
+        'Montmorency', 'France', 'Rencontre avec Luc'),
        ('customer2_id', 'beed1765-5c16-472a-b3f4-5c376ce5db58', 'Jean' ||
                                                                 ' Plombier', 'jean@email' ||
                                                                              '.com',
         '+33 12 34 56 78', 'https://jean.website.com', '4 Avenue des Près', 95160,
-        'Montmorency', 'France'),
+        'Montmorency', 'France', 'Rencontre avec le plombier'),
         ('customer3_id', 'beed1765-5c16-472a-b3f4-5c376ce5db58', 'Marc' ||
                                                                 ' Montagnier', 'marc@email' ||
                                                                              '.com',
         '+33 12 34 56 78', 'https://marc.website.com', '4 Avenue des Près', 95160,
-        'Montmorency', 'France');
+        'Montmorency', 'France', 'Nouvelle rencontre');

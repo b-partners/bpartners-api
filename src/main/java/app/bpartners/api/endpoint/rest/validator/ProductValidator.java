92280442e3f14bb3a53188fee216ed8a1d38de1b
Ryan Andriamahery <ryan@hei.school>
2023-02-07 11:18:55 +0300

    fix: set product vat percent to 0 by default

--- src/main/java/app/bpartners/api/endpoint/rest/validator/ProductValidator.java
+++ src/main/java/app/bpartners/api/endpoint/rest/validator/ProductValidator.java
@@ -3,9 +3,11 @@ package app.bpartners.api.endpoint.rest.validator;
 import app.bpartners.api.endpoint.rest.model.Product;
 import app.bpartners.api.model.exception.BadRequestException;
 import java.util.function.Consumer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ProductValidator implements Consumer<Product> {
 
   @Override
@@ -21,7 +23,10 @@ public class ProductValidator implements Consumer<Product> {
       message.append("Quantity is mandatory. ");
     }
     if (product.getVatPercent() == null) {
-      message.append("Vat percent is mandatory. ");
+      log.warn("DEPRECATED : Vat percent is mandatory. 0% by default is now attribute.");
+      //TODO: uncomment when any log is shown anymore
+      product.setVatPercent(0);
+      //message.append("Vat percent is mandatory. ");
     }
     String exceptionMessage = message.toString();
     if (!exceptionMessage.isEmpty()) {

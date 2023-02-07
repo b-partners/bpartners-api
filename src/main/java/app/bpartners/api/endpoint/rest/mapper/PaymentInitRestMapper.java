2fc71feb39c998d1df7acc51d0454ab7281d8454
Ryan Andriamahery <ryan@hei.school>
2023-02-07 17:45:00 +0300

    chore: remove reading payment request feat

--- src/main/java/app/bpartners/api/endpoint/rest/mapper/PaymentInitRestMapper.java
+++ src/main/java/app/bpartners/api/endpoint/rest/mapper/PaymentInitRestMapper.java
@@ -1,12 +1,9 @@
 package app.bpartners.api.endpoint.rest.mapper;
 
-import app.bpartners.api.endpoint.rest.model.Invoice;
 import app.bpartners.api.endpoint.rest.model.PaymentInitiation;
 import app.bpartners.api.endpoint.rest.model.PaymentRedirection;
-import app.bpartners.api.endpoint.rest.model.PaymentRequest;
 import app.bpartners.api.endpoint.rest.model.RedirectionStatusUrls;
 import app.bpartners.api.endpoint.rest.validator.PaymentInitValidator;
-import app.bpartners.api.repository.InvoiceRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +13,6 @@ import static app.bpartners.api.service.utils.FractionUtils.parseFraction;
 @AllArgsConstructor
 public class PaymentInitRestMapper {
   private final PaymentInitValidator paymentValidator;
-  private final InvoiceRestMapper invoiceRestMapper;
-  private final InvoiceRepository invoiceRepository;
 
   public app.bpartners.api.model.PaymentInitiation toDomain(PaymentInitiation rest) {
     RedirectionStatusUrls statusUrls = rest.getRedirectionStatusUrls();
@@ -32,26 +27,6 @@ public class PaymentInitRestMapper {
         .successUrl(statusUrls.getSuccessUrl())
         .failureUrl(statusUrls.getFailureUrl())
         .build();
-  }
-
-  public PaymentRequest toRest(app.bpartners.api.model.PaymentRequest domain) {
-    String invoiceId = domain.getInvoiceId();
-    Invoice invoice =
-        invoiceId == null ? null :
-            invoiceRestMapper.toRest(invoiceRepository.getById(invoiceId));
-    return new PaymentRequest()
-        .id(domain.getId())
-        .transferState(domain.getTransferState())
-        .paymentUrl(domain.getPaymentUrl())
-        .status(domain.getStatus())
-        .paymentScheme(domain.getPaymentScheme())
-        .label(domain.getLabel())
-        .invoice(invoice)
-        .endToEndId(domain.getSessionId())
-        .payerEmail(domain.getPayerEmail())
-        .payerName(domain.getPayerName())
-        .reference(domain.getReference())
-        .amount(domain.getAmount().getCentsRoundUp());
   }
 
   public PaymentRedirection toRest(app.bpartners.api.model.PaymentRedirection domain) {

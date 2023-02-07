17aabf6e28ae2fc4ba00e72ec89ac5845fb723a5
Ryan Andriamahery <ryan@hei.school>
2023-02-07 23:59:39 +0300

    chore: add transaction categories not subject to vat migration

--- src/test/java/app/bpartners/api/integration/TransactionCategoryIT.java
+++ src/test/java/app/bpartners/api/integration/TransactionCategoryIT.java
@@ -130,7 +130,7 @@ class TransactionCategoryIT {
     List<TransactionCategory> actualOutcome = api.getTransactionCategories(JOE_DOE_ACCOUNT_ID,
         LocalDate.now(), LocalDate.now(), OUTCOME);
 
-    assertEquals(32, actualAll.size());
+    assertEquals(34, actualAll.size());
     assertTrue(actualIncome.stream().allMatch(t -> Objects.equals(t.getTransactionType(), INCOME)));
     assertTrue(actualOutcome.stream().allMatch(t -> Objects.equals(t.getTransactionType(),
         OUTCOME)));
@@ -151,7 +151,7 @@ class TransactionCategoryIT {
     List<TransactionCategory> actualAll = api.getTransactionCategories(
         JOE_DOE_ACCOUNT_ID, startDate, endDate, null);
 
-    assertEquals(32, actualAll.size());
+    assertEquals(34, actualAll.size());
     assertTrue(actualAll.stream().noneMatch(e -> e.getCount() != 0L));
   }
 

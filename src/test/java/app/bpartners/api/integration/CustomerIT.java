bbf1cbb366f6a42b1ba28e7ecafea8a0d20a9c17
VAGNO Ny Hasina Marolahy <98644255+NyH1409@users.noreply.github.com>
2023-02-15 13:11:39 +0300

    feat: import customers from excel file

--- src/test/java/app/bpartners/api/integration/CustomerIT.java
+++ src/test/java/app/bpartners/api/integration/CustomerIT.java
@@ -18,17 +18,30 @@ import app.bpartners.api.repository.sendinblue.SendinblueConf;
 import app.bpartners.api.repository.swan.AccountHolderSwanRepository;
 import app.bpartners.api.repository.swan.AccountSwanRepository;
 import app.bpartners.api.repository.swan.UserSwanRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static app.bpartners.api.integration.conf.TestUtils.BAD_USER_ID;
+import static app.bpartners.api.integration.conf.TestUtils.BEARER_PREFIX;
 import static app.bpartners.api.integration.conf.TestUtils.JOE_DOE_ACCOUNT_ID;
+import static app.bpartners.api.integration.conf.TestUtils.JOE_DOE_TOKEN;
 import static app.bpartners.api.integration.conf.TestUtils.OTHER_ACCOUNT_ID;
 import static app.bpartners.api.integration.conf.TestUtils.OTHER_CUSTOMER_ID;
 import static app.bpartners.api.integration.conf.TestUtils.assertThrowsApiException;
@@ -44,6 +57,7 @@ import static app.bpartners.api.integration.conf.TestUtils.setUpSwanComponent;
 import static app.bpartners.api.integration.conf.TestUtils.setUpUserSwanRepository;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
@@ -76,7 +90,7 @@ class CustomerIT {
   private LegalFileRepository legalFileRepositoryMock;
 
   private static ApiClient anApiClient() {
-    return TestUtils.anApiClient(TestUtils.JOE_DOE_TOKEN, ContextInitializer.SERVER_PORT);
+    return TestUtils.anApiClient(JOE_DOE_TOKEN, ContextInitializer.SERVER_PORT);
   }
 
   @BeforeEach
@@ -194,11 +208,61 @@ class CustomerIT {
             List.of(customerUpdated().id(OTHER_CUSTOMER_ID))));
     assertThrowsApiException(
         "{\"type\":\"400 BAD_REQUEST\",\"message\":\"Identifier must not be null."
-            + " firstName not be null.\"}",
-        () -> api.updateCustomers(JOE_DOE_ACCOUNT_ID,
+            + " firstName not be null.\"}", () -> api.updateCustomers(JOE_DOE_ACCOUNT_ID,
             List.of(customerUpdated().id(null).firstName(null))));
     assertThrowsForbiddenException(
         () -> api.updateCustomers(OTHER_ACCOUNT_ID, List.of(customerUpdated())));
+  }
+
+  @Test
+  void create_customers_from_uploaded_file_ok() throws IOException, InterruptedException {
+    Resource filetoUpload = new ClassPathResource("files/customers.xlsx");
+
+    HttpResponse<String> response = uploadFile(JOE_DOE_ACCOUNT_ID, filetoUpload.getFile());
+    CollectionType playerListType = new ObjectMapper().getTypeFactory()
+        .constructCollectionType(List.class, Customer.class);
+    List<Customer> actual = new ObjectMapper().findAndRegisterModules()
+        .readValue(response.body(), playerListType);
+
+    assertNotNull(actual);
+    assertEquals(6, actual.size());
+  }
+
+  @Test
+  void create_customers_from_uploaded_file_ko() throws IOException, InterruptedException {
+    Resource file = new ClassPathResource("files/wrong-customers.xlsx");
+
+    HttpResponse<String> response = uploadFile(JOE_DOE_ACCOUNT_ID, file.getFile());
+
+    assertEquals(HttpStatus.BAD_REQUEST.value(), response.statusCode());
+    assertEquals(
+        "{\"type\":\"400 BAD_REQUEST\","
+            + "\"message\":\"\"Nom\" instead of \"lastname\" at column 1. "
+            + "\"Prénom(s)\" instead of \"firstname\" at column 2. "
+            + "\"Email\" instead of \"mail\" at column 3. "
+            + "\"Téléphone\" instead of \"phone\" at column 4. "
+            + "\"Siteweb\" instead of \"site\" at column 5. "
+            + "\"Adresse\" instead of \"address\" at column 6. "
+            + "\"Code Postal\" instead of \"code\" at column 7. "
+            + "\"Ville\" instead of \"city\" at column 8. "
+            + "\"Pays\" instead of \"country\" at column 9. "
+            + "\"Commentaires\" instead of \"comments\" at the last column."
+            + "\"}", response.body().replace("\\", "")
+    );
+  }
+
+  private HttpResponse<String> uploadFile(String accountId, File toUpload)
+      throws IOException, InterruptedException {
+    HttpClient unauthenticatedClient = HttpClient.newBuilder().build();
+    String basePath = "http://localhost:" + CustomerIT.ContextInitializer.SERVER_PORT;
+
+    HttpResponse<String> response = unauthenticatedClient.send(HttpRequest.newBuilder()
+            .uri(URI.create(basePath + "/accounts/" + accountId + "/customers/upload"))
+            .header("Authorization", BEARER_PREFIX + JOE_DOE_TOKEN)
+            .method("POST", HttpRequest.BodyPublishers.ofFile(toUpload.toPath())).build(),
+        HttpResponse.BodyHandlers.ofString());
+
+    return response;
   }
 
   public static class ContextInitializer extends AbstractContextInitializer {

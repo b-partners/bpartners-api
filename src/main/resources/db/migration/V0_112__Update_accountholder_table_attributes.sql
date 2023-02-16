8178bc94a6b7c06e25d0e88efca368df3f270c64
Onitsiky <hei.onitsiky@gmail.com>
2023-02-16 21:28:45 +0300

    chore: update account holder table attributes and test datas

---
+++ src/main/resources/db/migration/V0_112__Update_accountholder_table_attributes.sql
@@ -0,0 +1,20 @@
+do
+$$
+    begin
+        if not exists(select from pg_type where typname = 'verification_status') then
+            create type verification_status as enum ('VERIFIED', 'PENDING', 'NOT_STARTED', 'WAITING_FOR_INFORMATION');
+        end if;
+    end
+$$;
+
+alter table "account_holder" add column verification_status verification_status;
+alter table "account_holder" add column "name" varchar;
+alter table "account_holder" add column registration_number varchar;
+alter table "account_holder" add column business_activity varchar;
+alter table "account_holder" add column business_activity_description varchar;
+alter table "account_holder" add column address_line1 varchar;
+alter table "account_holder" add column city varchar;
+alter table "account_holder" add column country varchar;
+alter table "account_holder" add column postal_code varchar;
+alter table "account_holder"
+    add constraint account_fk foreign key (account_id) references account(id);
--- src/main/resources/db/testdata/V99_13__Test_create_account_holder.sql
+++ src/main/resources/db/testdata/V99_13__Test_create_account_holder.sql
@@ -1,6 +1,9 @@
 insert into "account_holder"
 (id, mobile_phone_number, account_id, email, social_capital, tva_number, initial_cashflow,
- subject_to_vat)
+ subject_to_vat, verification_status, "name", registration_number, business_activity,
+ business_activity_description, address_line1, city, country, postal_code)
 values ('b33e6eb0-e262-4596-a91f-20c6a7bfd343', '+33 6 11 22 33 44',
         'beed1765-5c16-472a-b3f4-5c376ce5db58',
-        'numer@hei.school', 40000, 'FR 32 123456789', '6000/1', true);
+        'numer@hei.school', 40000, 'FR 32 123456789', '6000/1', true,
+        'PENDING','John Doe', '12345', 'Administrative service', 'Plombier', 'Lance', 'Paris',
+        'France', '14');

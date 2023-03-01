c371b374794a626d99b4480b7561a0ca3ca33a74
Onitsiky <hei.onitsiky@gmail.com>
2023-03-01 15:31:40 +0300

    fix: do not ignore status in ProductRepo::byIdAccountAndStatus

--- src/main/resources/db/testdata/V99_13__Test_create_account_holder.sql
+++ src/main/resources/db/testdata/V99_13__Test_create_account_holder.sql
@@ -6,4 +6,7 @@ values ('b33e6eb0-e262-4596-a91f-20c6a7bfd343', '+33 6 11 22 33 44',
         'beed1765-5c16-472a-b3f4-5c376ce5db58',
         'numer@hei.school', 40000, 'FR32123456789', '6000/1', true,
         'VERIFIED','NUMER', '899067250', 'businessAndRetail', 'Plombier', '6 RUE PAUL LANGEVIN', 'FONTENAY-SOUS-BOIS',
-        'FRA', '94120');
+        'FRA', '94120'),
+       ('account_holder_id_2', '+33 6 13 22 33 44',
+        'jane_account_id',
+        'jane@hei.school', 40000, 'FR 31 123456789', '5000/1', true);

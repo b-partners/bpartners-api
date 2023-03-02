c14fb3d8d60eb69649597b73be0e0320dda3fdf3
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2023-03-02 09:29:27 +0300

    chore: implement update prospect

--- src/main/java/app/bpartners/api/endpoint/rest/mapper/ProspectRestMapper.java
+++ src/main/java/app/bpartners/api/endpoint/rest/mapper/ProspectRestMapper.java
@@ -1,29 +1,34 @@
 package app.bpartners.api.endpoint.rest.mapper;
 
 import app.bpartners.api.endpoint.rest.model.Prospect;
+import app.bpartners.api.endpoint.rest.model.UpdateProspect;
+import app.bpartners.api.endpoint.rest.validator.ProspectRestValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
 public class ProspectRestMapper {
+  private final ProspectRestValidator validator;
+
   public Prospect toRest(app.bpartners.api.model.Prospect domain) {
     return new Prospect()
         .id(domain.getId())
         .email(domain.getEmail())
         .name(domain.getName())
         .phone(domain.getPhone())
-        .location(domain.getLocation())
+        .address(domain.getAddress())
         .status(domain.getStatus());
   }
 
-  public app.bpartners.api.model.Prospect toDomain(Prospect rest) {
+  public app.bpartners.api.model.Prospect toDomain(UpdateProspect rest) {
+    validator.accept(rest);
     return app.bpartners.api.model.Prospect.builder()
         .id(rest.getId())
         .email(rest.getEmail())
         .name(rest.getName())
         .phone(rest.getPhone())
-        .location(rest.getLocation())
+        .address(rest.getAddress())
         .status(rest.getStatus())
         .build();
   }
---
+++ src/test/java/app/bpartners/api/unit/validator/ProspectRestValidatorTest.java
@@ -0,0 +1,39 @@
+package app.bpartners.api.unit.validator;
+
+import app.bpartners.api.endpoint.rest.model.UpdateProspect;
+import app.bpartners.api.endpoint.rest.validator.ProspectRestValidator;
+import org.junit.jupiter.api.Test;
+
+import static app.bpartners.api.endpoint.rest.model.ProspectStatus.TO_CONTACT;
+import static app.bpartners.api.integration.conf.TestUtils.assertThrowsBadRequestException;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class ProspectRestValidatorTest {
+  private final ProspectRestValidator subject = new ProspectRestValidator();
+
+  UpdateProspect validProspect() {
+    return new UpdateProspect()
+        .id("valid_id")
+        .name("valid_name")
+        .email("johnDoe@email.com")
+        .phone("+33611223344")
+        .address("valid_address")
+        .status(TO_CONTACT);
+  }
+
+  @Test
+  void subject_accept_valid_prospect_ok() {
+    assertDoesNotThrow(() -> subject.accept(validProspect()));
+    assertDoesNotThrow(() -> subject.accept(validProspect().email(null)));
+    assertDoesNotThrow(() -> subject.accept(validProspect().phone(null)));
+  }
+
+  @Test
+  void subject_reject_invalid_prospect_ko() {
+    assertThrowsBadRequestException("Id is mandatory. "
+            + "Name is mandatory. "
+            + "Status is mandatory. "
+            + "Prospect should at least have a phone number or an email. ",
+        () -> subject.accept(new UpdateProspect()));
+  }
+}

b4e6865d9c308a23ceb600ec612a8de13556b4d8
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2023-03-09 19:19:33 +0300

    chore: get prospects from building permits

--- src/main/java/app/bpartners/api/repository/implementation/ProspectRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/implementation/ProspectRepositoryImpl.java
@@ -3,8 +3,11 @@ package app.bpartners.api.repository.implementation;
 import app.bpartners.api.model.Prospect;
 import app.bpartners.api.model.mapper.ProspectMapper;
 import app.bpartners.api.repository.ProspectRepository;
+import app.bpartners.api.repository.SogefiBuildingPermitRepository;
 import app.bpartners.api.repository.jpa.ProspectJpaRepository;
 import app.bpartners.api.repository.jpa.model.HProspect;
+import app.bpartners.api.repository.prospecting.datasource.buildingpermit.BuildingPermitApi;
+import app.bpartners.api.repository.prospecting.datasource.buildingpermit.model.SingleBuildingPermit;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
@@ -19,27 +22,41 @@ import static org.springframework.transaction.annotation.Isolation.SERIALIZABLE;
 public class ProspectRepositoryImpl implements ProspectRepository {
   private final ProspectJpaRepository jpaRepository;
   private final ProspectMapper mapper;
-  // TODO : use classes from prospecting.datasource package
-  //  to get prospects
+  private final BuildingPermitApi buildingPermitApi;
+  private final SogefiBuildingPermitRepository sogefiBuildingPermitRepository;
 
+  //todo: an accountholder should get new prospects if he prospects from sogefi and if he needs
+  // prospects
   @Override
   public List<Prospect> findAllByIdAccountHolder(String idAccountHolder) {
+    boolean isSogefiProspector = true;
+    if (isSogefiProspector) {
+      buildingPermitApi.getData().getRecords()
+          .forEach(
+              buildingPermit -> {
+                SingleBuildingPermit singleBuildingPermit =
+                    buildingPermitApi.getOne(String.valueOf(buildingPermit.getFileId()));
+                sogefiBuildingPermitRepository.saveByBuildingPermit(idAccountHolder,
+                    buildingPermit, singleBuildingPermit);
+              });
+    }
     return jpaRepository.findAllByIdAccountHolder(idAccountHolder)
         .stream()
-        .map(mapper::toDomain)
+        .map(prospect -> mapper.toDomain(prospect, isSogefiProspector))
         .collect(Collectors.toUnmodifiableList());
   }
 
   @Transactional(isolation = SERIALIZABLE)
   @Override
   public List<Prospect> saveAll(List<Prospect> prospects) {
+    boolean isSogefiProspector = true;
     List<HProspect> entities = prospects
         .stream()
         .map(mapper::toEntity)
         .collect(Collectors.toUnmodifiableList());
     return jpaRepository.saveAll(entities)
         .stream()
-        .map(mapper::toDomain)
+        .map(entity -> mapper.toDomain(entity, isSogefiProspector))
         .collect(Collectors.toUnmodifiableList());
   }
 }

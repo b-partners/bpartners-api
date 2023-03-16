5d6b1ec626a24911ba18985c1f8b1110eca6afff
dinasoa <hei.dinasoa@gmail.com>
2023-03-16 10:33:45 +0300

    chore: check should be silent for a while if amountAttempted is more than expected

--- src/main/java/app/bpartners/api/repository/implementation/ProspectRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/implementation/ProspectRepositoryImpl.java
@@ -2,8 +2,11 @@ package app.bpartners.api.repository.implementation;
 
 import app.bpartners.api.endpoint.rest.security.AuthenticatedResourceProvider;
 import app.bpartners.api.model.AccountHolder;
+import app.bpartners.api.model.AnnualRevenueTarget;
 import app.bpartners.api.model.BusinessActivity;
+import app.bpartners.api.model.Fraction;
 import app.bpartners.api.model.Prospect;
+import app.bpartners.api.model.TransactionsSummary;
 import app.bpartners.api.model.mapper.ProspectMapper;
 import app.bpartners.api.repository.ProspectRepository;
 import app.bpartners.api.repository.SogefiBuildingPermitRepository;
@@ -11,14 +14,25 @@ import app.bpartners.api.repository.jpa.ProspectJpaRepository;
 import app.bpartners.api.repository.jpa.model.HProspect;
 import app.bpartners.api.repository.prospecting.datasource.buildingpermit.BuildingPermitApi;
 import app.bpartners.api.repository.prospecting.datasource.buildingpermit.model.SingleBuildingPermit;
+import app.bpartners.api.service.AccountHolderService;
+import app.bpartners.api.service.AnnualRevenueTargetService;
 import app.bpartners.api.service.BusinessActivityService;
+import app.bpartners.api.service.ProspectService;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import static app.bpartners.api.service.utils.FractionUtils.parseFraction;
 import static org.springframework.transaction.annotation.Isolation.SERIALIZABLE;
 
 @AllArgsConstructor
@@ -33,7 +47,10 @@ public class ProspectRepositoryImpl implements ProspectRepository {
   private final SogefiBuildingPermitRepository sogefiBuildingPermitRepository;
   private final BusinessActivityService businessActivityService;
   private final AuthenticatedResourceProvider resourceProvider;
-
+  private final AccountHolderService accountHolder;
+  private final AnnualRevenueTargetService revenueTargetService;
+  private  final TransactionsSummaryRepositoryImpl transactionsSummaryRepository;
+  private final ProspectService prospectService;
   @Override
   public List<Prospect> findAllByIdAccountHolder(String idAccountHolder) {
     boolean isSogefiProspector = isSogefiProspector(idAccountHolder);
@@ -50,7 +67,7 @@ public class ProspectRepositoryImpl implements ProspectRepository {
         .collect(Collectors.toUnmodifiableList());
   }
 
-  private boolean isSogefiProspector(String idAccountHolder) {
+  public boolean isSogefiProspector(String idAccountHolder) {
     BusinessActivity businessActivity =
         businessActivityService.findByAccountHolderId(idAccountHolder);
     return Objects.equals(0, TILE_LAYER.compareToIgnoreCase(businessActivity.getPrimaryActivity()))
@@ -71,5 +88,39 @@ public class ProspectRepositoryImpl implements ProspectRepository {
     return jpaRepository.saveAll(entities).stream()
         .map(entity -> mapper.toDomain(entity, isSogefiProspector))
         .collect(Collectors.toUnmodifiableList());
+  }
+
+  @Override
+  public boolean needsProspects(String idAccountHolder) {
+    Optional<AnnualRevenueTarget> revenueTargetsInAYear =
+        revenueTargetService.getByYear(idAccountHolder, Year.now().getValue());
+
+    if (revenueTargetsInAYear.isEmpty()) {
+      return false;
+    }
+
+    Instant instant = revenueTargetsInAYear.get().getUpdatedAt();
+    LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+    int dayNumber = localDateTime.getDayOfYear();
+
+    Fraction expectedAmountAttemptedAtCurrentDay =
+        parseFraction(revenueTargetsInAYear.get().getAmountTarget().getCentsRoundUp() * dayNumber / 365);
+    Fraction amountAttemptedActual =
+        parseFraction(revenueTargetsInAYear.get().getAmountAttempted().getCentsRoundUp());
+
+
+    if(amountAttemptedActual.getCentsRoundUp() > expectedAmountAttemptedAtCurrentDay.getCentsRoundUp()){
+      int extraMoney =
+          (revenueTargetsInAYear.get().getAmountAttempted().getCentsRoundUp() * 100 / 10000) -
+              (expectedAmountAttemptedAtCurrentDay.getCentsRoundUp() / 10000);
+      int day =
+          (extraMoney * 365 / revenueTargetsInAYear.get().getAmountTarget().getCentsRoundUp()) / 100;
+      LocalDateTime dateTime = LocalDateTime.now().plusDays(day);
+      prospectService.setEnabled(false);
+      prospectService.setSilentUntil(dateTime);
+    }
+
+    return amountAttemptedActual.getCentsRoundUp() <
+        expectedAmountAttemptedAtCurrentDay.getCentsRoundUp();
   }
 }
--- src/main/java/app/bpartners/api/service/ProspectService.java
+++ src/main/java/app/bpartners/api/service/ProspectService.java
@@ -1,17 +1,36 @@
 package app.bpartners.api.service;
 
 import app.bpartners.api.model.Prospect;
+import app.bpartners.api.model.exception.ApiException;
 import app.bpartners.api.repository.ProspectRepository;
+import app.bpartners.api.repository.jpa.AccountHolderJpaRepository;
+import app.bpartners.api.repository.jpa.model.HAccountHolder;
+import app.bpartners.api.service.aws.SesService;
 import app.bpartners.api.service.dataprocesser.ProspectDataProcesser;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
+import javax.mail.MessagingException;
 import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+
+import static app.bpartners.api.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
+import static app.bpartners.api.service.utils.TemplateResolverUtils.parseTemplateResolver;
 
 @Service
 @AllArgsConstructor
+@Setter
 public class ProspectService {
+  public static final String PROSPECT_MAIL_TEMPLATE = "prospect_mail";
   private final ProspectRepository repository;
   private final ProspectDataProcesser dataProcesser;
+  private final AccountHolderJpaRepository accountHolderJpaRepository;
+  private final SesService sesService;
+  private boolean enabled = true;
+  private LocalDateTime silentUntil = null;
 
   public List<Prospect> getAllByIdAccountHolder(String idAccountHolder) {
     return dataProcesser.processProspects(repository.findAllByIdAccountHolder(idAccountHolder));
@@ -19,5 +38,27 @@ public class ProspectService {
 
   public List<Prospect> saveAll(List<Prospect> toCreate) {
     return repository.saveAll(toCreate);
+  }
+
+  @Scheduled(cron = "0 0 10 * * *")
+  public void prospect() {
+    if (enabled && (silentUntil == null || LocalDateTime.now().isAfter(silentUntil))) {
+      accountHolderJpaRepository.findAll().forEach(accountHolder -> {
+        if (repository.needsProspects(accountHolder.getId()) && repository.isSogefiProspector(accountHolder.getId())) {
+          final String subject = "Avez-vous besoin de nouveaux clients ?";
+          final String htmlbody = parseTemplateResolver(PROSPECT_MAIL_TEMPLATE, configureProspectContext(accountHolder));
+          try {
+            sesService.sendEmail(accountHolder.getEmail(), subject, htmlbody, List.of());
+          } catch (IOException | MessagingException e) {
+            throw new ApiException(SERVER_EXCEPTION, e);
+          }
+        }
+      });
+    }
+  }
+  private Context configureProspectContext(HAccountHolder accountHolder) {
+    Context context = new Context();
+    context.setVariable("accountHolderEntity", accountHolder);
+    return context;
   }
 }

afc700cac8d46db5b4f4231bdf6a5c612c301f28
dinasoa <hei.dinasoa@gmail.com>
2023-03-16 10:33:45 +0300

    feat: send email to accountHolders when they’re late on their revenue target
    chore: remove useless conditions

--- src/main/java/app/bpartners/api/repository/implementation/ProspectRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/implementation/ProspectRepositoryImpl.java
@@ -2,7 +2,9 @@ package app.bpartners.api.repository.implementation;
 
 import app.bpartners.api.endpoint.rest.security.AuthenticatedResourceProvider;
 import app.bpartners.api.model.AccountHolder;
+import app.bpartners.api.model.AnnualRevenueTarget;
 import app.bpartners.api.model.BusinessActivity;
+import app.bpartners.api.model.Fraction;
 import app.bpartners.api.model.Prospect;
 import app.bpartners.api.model.exception.BadRequestException;
 import app.bpartners.api.model.mapper.ProspectMapper;
@@ -13,19 +15,25 @@ import app.bpartners.api.repository.jpa.ProspectJpaRepository;
 import app.bpartners.api.repository.jpa.model.HProspect;
 import app.bpartners.api.repository.prospecting.datasource.buildingpermit.BuildingPermitApi;
 import app.bpartners.api.repository.prospecting.datasource.buildingpermit.model.SingleBuildingPermit;
+import app.bpartners.api.service.AnnualRevenueTargetService;
 import app.bpartners.api.service.BusinessActivityService;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Year;
+import java.time.ZoneId;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import static app.bpartners.api.service.utils.FractionUtils.parseFraction;
 import static org.springframework.transaction.annotation.Isolation.SERIALIZABLE;
 
 @AllArgsConstructor
 @Repository
-
 public class ProspectRepositoryImpl implements ProspectRepository {
   public static final String TILE_LAYER = "carreleur";
   public static final String ROOFER = "toiturier";
@@ -35,6 +43,7 @@ public class ProspectRepositoryImpl implements ProspectRepository {
   private final SogefiBuildingPermitRepository sogefiBuildingPermitRepository;
   private final BusinessActivityService businessActivityService;
   private final AuthenticatedResourceProvider resourceProvider;
+  private final AnnualRevenueTargetService revenueTargetService;
   private final AccountHolderRepository accountHolderRepository;
 
   @Override
@@ -60,7 +69,7 @@ public class ProspectRepositoryImpl implements ProspectRepository {
         .collect(Collectors.toUnmodifiableList());
   }
 
-  private boolean isSogefiProspector(String idAccountHolder) {
+  public boolean isSogefiProspector(String idAccountHolder) {
     BusinessActivity businessActivity =
         businessActivityService.findByAccountHolderId(idAccountHolder);
     return Objects.equals(0, TILE_LAYER.compareToIgnoreCase(businessActivity.getPrimaryActivity()))
@@ -81,5 +90,27 @@ public class ProspectRepositoryImpl implements ProspectRepository {
     return jpaRepository.saveAll(entities).stream()
         .map(entity -> mapper.toDomain(entity, isSogefiProspector))
         .collect(Collectors.toUnmodifiableList());
+  }
+
+  @Override
+  public boolean needsProspects(String idAccountHolder) {
+    Optional<AnnualRevenueTarget> revenueTargetsInAYear =
+        revenueTargetService.getByYear(idAccountHolder, Year.now().getValue());
+
+    if (revenueTargetsInAYear.isEmpty()) {
+      return false;
+    }
+
+    Instant instant = revenueTargetsInAYear.get().getUpdatedAt();
+    LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+    int currentDayNumber = localDateTime.getDayOfYear();
+
+    Fraction expectedAmountAttemptedAtCurrentDay = parseFraction(
+        revenueTargetsInAYear.get().getAmountTarget().getCentsRoundUp() * currentDayNumber / 365);
+    Fraction amountAttemptedActual =
+        parseFraction(revenueTargetsInAYear.get().getAmountAttempted().getCentsRoundUp());
+
+    return amountAttemptedActual.getCentsRoundUp()
+        < expectedAmountAttemptedAtCurrentDay.getCentsRoundUp();
   }
 }
---
+++ src/test/java/app/bpartners/api/unit/service/ProspectServiceTest.java
@@ -0,0 +1,54 @@
+package app.bpartners.api.unit.service;
+
+import app.bpartners.api.repository.ProspectRepository;
+import app.bpartners.api.repository.jpa.AccountHolderJpaRepository;
+import app.bpartners.api.repository.jpa.model.HAccountHolder;
+import app.bpartners.api.service.ProspectService;
+import app.bpartners.api.service.aws.SesService;
+import app.bpartners.api.service.dataprocesser.ProspectDataProcesser;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+import javax.mail.MessagingException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static app.bpartners.api.integration.conf.TestUtils.SWAN_ACCOUNTHOLDER_ID;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class ProspectServiceTest {
+  ProspectRepository prospectRepositoryMock = mock(ProspectRepository.class);
+  ProspectDataProcesser dataProcesserMock = mock(ProspectDataProcesser.class);
+  AccountHolderJpaRepository accountHolderJpaRepositoryMock =
+      mock(AccountHolderJpaRepository.class);
+  SesService sesServiceMock = mock(SesService.class);
+  ProspectService subject = new ProspectService(prospectRepositoryMock, dataProcesserMock,
+      accountHolderJpaRepositoryMock, sesServiceMock);
+
+  @BeforeEach
+  void setup() {
+    when(accountHolderJpaRepositoryMock.findAll()).thenReturn(
+        List.of(HAccountHolder.builder()
+                .id(SWAN_ACCOUNTHOLDER_ID)
+                .build(),
+            HAccountHolder.builder()
+                .id("fake_accountholder_id")
+                .build()));
+    when(prospectRepositoryMock.needsProspects(any()))
+        .thenAnswer(i -> Objects.equals(i.getArgument(0), SWAN_ACCOUNTHOLDER_ID));
+    when(prospectRepositoryMock.isSogefiProspector(any()))
+        .thenAnswer(i -> Objects.equals(i.getArgument(0), SWAN_ACCOUNTHOLDER_ID));
+  }
+
+  @Test
+  void test_scheduler_ok() throws MessagingException, IOException {
+    subject.prospect();
+
+    verify(sesServiceMock, times(1)).sendEmail(any(), any(), any(), any());
+  }
+}

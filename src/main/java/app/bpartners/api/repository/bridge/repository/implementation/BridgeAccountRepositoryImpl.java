2f01666a21c235866577c2bab5e3b6cd70466829
Ryan Andriamahery <ryan@hei.school>
2023-04-04 17:13:26 +0300

    fix: choose unique default account from bridge when multiples are provided

--- src/main/java/app/bpartners/api/repository/bridge/repository/implementation/BridgeAccountRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/bridge/repository/implementation/BridgeAccountRepositoryImpl.java
@@ -6,31 +6,53 @@ import app.bpartners.api.repository.bridge.model.Account.BridgeAccount;
 import app.bpartners.api.repository.bridge.repository.BridgeAccountRepository;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @AllArgsConstructor
+@Slf4j
 public class BridgeAccountRepositoryImpl implements BridgeAccountRepository {
   private final BridgeApi bridgeApi;
 
   @Override
   public List<BridgeAccount> findByBearer(String bearer) {
     List<BridgeAccount> accounts = bridgeApi.findAccountsByToken(bearer);
-    //TODO: uncomment when testing in prod
-    //if (accounts.size() >= 2) {
-    //log.warn("Only one supported account for now");
-    //return List.of();
-    //}
+    //TODO: choose what account should be used
+    if (accounts.size() > 1) {
+      StringBuilder builder = getAccountMessageBuilder(accounts);
+      log.warn(
+          "[Bridge] Only one account is supported for now. "
+              + "Therefore, these accounts were found :" + builder);
+    }
     return accounts.isEmpty() ? List.of() : List.of(accounts.get(0));
   }
 
   @Override
   public List<BridgeAccount> findAllByAuthenticatedUser() {
-    return bridgeApi.findAccountsByToken(AuthProvider.getPrincipal().getBearer());
+    return findByBearer(AuthProvider.getPrincipal().getBearer());
   }
 
   @Override
   public BridgeAccount findById(Long id) {
     return bridgeApi.findByAccountById(id, AuthProvider.getPrincipal().getBearer());
+  }
+
+  private StringBuilder getAccountMessageBuilder(List<BridgeAccount> accounts) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < accounts.size(); i++) {
+      BridgeAccount bridgeAccount = accounts.get(i);
+      builder.append("Account(id=")
+          .append(bridgeAccount.getId())
+          .append(",name=")
+          .append(bridgeAccount.getName())
+          .append(",status=")
+          .append(bridgeAccount.getStatus())
+          .append(")");
+      if (i != accounts.size() - 1) {
+        builder.append(" and ");
+      }
+    }
+    return builder;
   }
 }

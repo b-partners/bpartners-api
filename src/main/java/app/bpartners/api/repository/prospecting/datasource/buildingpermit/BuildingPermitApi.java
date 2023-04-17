c4ef7893284bea7e399f6b763cb121f1b9d2e1d4
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2023-04-17 09:30:50 +0300

    fixup: put retry as a todo

--- src/main/java/app/bpartners/api/repository/prospecting/datasource/buildingpermit/BuildingPermitApi.java
+++ src/main/java/app/bpartners/api/repository/prospecting/datasource/buildingpermit/BuildingPermitApi.java
@@ -3,6 +3,7 @@ package app.bpartners.api.repository.prospecting.datasource.buildingpermit;
 import app.bpartners.api.model.exception.ApiException;
 import app.bpartners.api.repository.prospecting.datasource.buildingpermit.model.BuildingPermitList;
 import app.bpartners.api.repository.prospecting.datasource.buildingpermit.model.SingleBuildingPermit;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.URI;
@@ -61,6 +62,10 @@ public class BuildingPermitApi {
       log.info("SOGEFI CALL - id={}, InterruptedException", requestId);
       Thread.currentThread().interrupt();
       throw new ApiException(SERVER_EXCEPTION, e);
+    } catch (JsonProcessingException e) {
+      log.info("SOGEFI CALL - id={}, MappingException", requestId);
+      //TODO: retry
+      return null;
     } catch (IOException e) {
       log.info("SOGEFI CALL - id={}, IOException", requestId);
       throw new ApiException(SERVER_EXCEPTION, e.getMessage());

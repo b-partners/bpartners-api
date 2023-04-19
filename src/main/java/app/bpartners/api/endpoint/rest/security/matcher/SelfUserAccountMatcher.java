174620c5712afff11b62028c0f683b7b18289f96
Ryan Andriamahery <ryan@hei.school>
2023-04-19 10:12:19 +0300

    fix(bridge): account integration test

--- src/main/java/app/bpartners/api/endpoint/rest/security/matcher/SelfUserAccountMatcher.java
+++ src/main/java/app/bpartners/api/endpoint/rest/security/matcher/SelfUserAccountMatcher.java
@@ -2,17 +2,18 @@ package app.bpartners.api.endpoint.rest.security.matcher;
 
 import app.bpartners.api.endpoint.rest.security.AuthProvider;
 import app.bpartners.api.endpoint.rest.security.AuthenticatedResourceProvider;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 @AllArgsConstructor
+@Slf4j
 public class SelfUserAccountMatcher implements RequestMatcher {
   //TODO: before setting up another selfMatcher with multiple group_names,
   // create an abstract class like SelfMatcher
@@ -35,12 +36,8 @@ public class SelfUserAccountMatcher implements RequestMatcher {
     var selfAccountId = getSelfAccountId(request);
     var principalUserId = AuthProvider.getPrincipal().getUserId();
     var principalAccountId = authResourceProvider.getAccount().getId();
-    //TODO(distinct-cognito): CRITICAL, this is just to make AccountIT::concurrently_get_bridge_account_holders pass
-    if ("c15924bf-61f9-4381-8c9b-d34369bf91f7".equals(principalAccountId) &&
-        "joe_doe_id".equals(principalUserId)) {
-      return true;
-    }
-    return Objects.equals(selfUserId, principalUserId) && Objects.equals(selfAccountId, principalAccountId);
+    return Objects.equals(selfUserId, principalUserId)
+        && Objects.equals(selfAccountId, principalAccountId);
   }
 
   private String getSelfUserId(HttpServletRequest request) {

863d101c096161cca0a3f45c6f69d29a3ad17a38
Onitsiky <hei.onitsiky@gmail.com>
2023-04-27 23:25:13 +0300

    feat: add script to create function for getting annual invoicing info

--- src/main/resources/db/migration/V0_154__Add_invoicing_info_function.sql
+++ src/main/resources/db/migration/V0_154__Add_invoicing_info_function.sql
@@ -70,3 +70,165 @@ BEGIN
 end;
 $$
     language plpgsql;
+
+create or replace function get_annual_invoicing_info(year integer)
+    returns table
+            (
+                account_id varchar,
+                name       varchar,
+                bic        varchar,
+                iban       varchar,
+                january    numeric,
+                february   numeric,
+                march      numeric,
+                april      numeric,
+                may        numeric,
+                june       numeric,
+                july       numeric,
+                august     numeric,
+                september  numeric,
+                october    numeric,
+                november   numeric,
+                december   numeric
+            )
+as
+$$
+DECLARE
+    rec RECORD;
+BEGIN
+    create temporary table annual_invoicing_info
+    (
+        account_id varchar,
+        name       varchar,
+        bic        varchar,
+        iban       varchar,
+        january    numeric default 0,
+        february   numeric default 0,
+        march      numeric default 0,
+        april      numeric default 0,
+        may        numeric default 0,
+        june       numeric default 0,
+        july       numeric default 0,
+        august     numeric default 0,
+        september  numeric default 0,
+        october    numeric default 0,
+        november   numeric default 0,
+        december   numeric default 0
+    );
+    insert into annual_invoicing_info (account_id, name, bic, iban)
+    select distinct pr.account_id, a.name, a.bic, a.iban
+    from payment_request pr
+             inner join account a on a.id = pr.account_id;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 1)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set january =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.january end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 2)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set february =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.february end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 3)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set march =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.march end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 4)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set april =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.april end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 5)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set may =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.may end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 6)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set june =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.june end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 7)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set july =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.july end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 8)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set august =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.august end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 9)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set september =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.september end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 10)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set october =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.october end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 11)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set november =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.november end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_invoicing_info_by_year_and_month(year, 12)
+        LOOP
+            UPDATE annual_invoicing_info as a
+            set december =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.december end
+            where a.account_id = rec.account_id;
+        end loop;
+    return query select * from annual_invoicing_info;
+    drop table annual_invoicing_info;
+end;
+$$
+    language plpgsql;

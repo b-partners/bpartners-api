380239ea158c40158224a7ad7f82fb6faad5ea73
Onitsiky <hei.onitsiky@gmail.com>
2023-05-05 14:33:31 +0300

    feat: add script to create function for getting billing info
    * feat: add script to create function for getting invoicing info
    
    * feat: add script to create function for getting invoicing info by year and month
    
    * feat: add script to create function for getting annual invoicing info
    
    * review: split function into tiny functions
    
    * review: refactor scripts
    
    * review: refactor scripts
    
    * review: refactor scripts
    
    * fix: file name

---
+++ src/main/resources/db/migration/V0_160__Get_annual_billing_info.sql
@@ -0,0 +1,140 @@
+create or replace function get_annual_billing_info(year integer)
+    returns table
+            (
+                account_id varchar,
+                name       varchar,
+                bic        varchar,
+                iban       varchar,
+                january    numeric,
+                february   numeric,
+                march      numeric,
+                april      numeric,
+                may        numeric,
+                june       numeric,
+                july       numeric,
+                august     numeric,
+                september  numeric,
+                october    numeric,
+                november   numeric,
+                december   numeric
+            )
+as
+$$
+DECLARE
+    rec RECORD;
+BEGIN
+    perform create_annual_billing_info_table();
+    perform insert_account_info_in_temp_table();
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 1)
+        LOOP
+            UPDATE annual_billing_info as a
+            set january =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.january end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 2)
+        LOOP
+            UPDATE annual_billing_info as a
+            set february =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.february end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 3)
+        LOOP
+            UPDATE annual_billing_info as a
+            set march =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.march end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 4)
+        LOOP
+            UPDATE annual_billing_info as a
+            set april =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.april end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 5)
+        LOOP
+            UPDATE annual_billing_info as a
+            set may =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.may end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 6)
+        LOOP
+            UPDATE annual_billing_info as a
+            set june =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.june end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 7)
+        LOOP
+            UPDATE annual_billing_info as a
+            set july =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.july end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 8)
+        LOOP
+            UPDATE annual_billing_info as a
+            set august =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.august end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 9)
+        LOOP
+            UPDATE annual_billing_info as a
+            set september =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.september end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 10)
+        LOOP
+            UPDATE annual_billing_info as a
+            set october =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.october end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 11)
+        LOOP
+            UPDATE annual_billing_info as a
+            set november =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.november end
+            where a.account_id = rec.account_id;
+        end loop;
+    FOR rec in select * from get_billing_info_by_year_and_month(year, 12)
+        LOOP
+            UPDATE annual_billing_info as a
+            set december =
+                    case
+                        when rec.account_id is not null then rec.amount_value
+                        else a.december end
+            where a.account_id = rec.account_id;
+        end loop;
+    return query select * from annual_billing_info;
+    drop table annual_billing_info;
+end;
+$$
+language plpgsql;

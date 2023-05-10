68be57b1a2def1153e67d47a3eb53306049edb7f
onitsiky <hei.onitsiky@gmail.com>
2023-05-10 16:53:06 +0300

    review: refactor script

--- src/main/resources/db/migration/V0_165__Delete_duplicated_transactions.sql
+++ src/main/resources/db/migration/V0_165__Delete_duplicated_transactions.sql
@@ -1,12 +1,12 @@
 delete
 from transaction tr
-where tr.id in (select min(v.id)
-                from (select *
+where tr.id in (select min(req2.id)
+                from (select t.id, t.id_bridge
                       from transaction t
                       where t.id_bridge in (select id_bridge
                                             from (select count(*), id_bridge from transaction group by id_bridge)
-                                                     as u
-                                            where u.count > 1)
-                         or id_bridge is null) as v
-                group by v.id_bridge)
+                                                     as req1
+                                            where req1.count > 1)
+                         or id_bridge is null) as req2
+                group by req2.id_bridge)
    or tr.id_bridge is null;

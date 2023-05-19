8f051714a584ad45a08d24c4dea872df0d156e63
onitsiky <hei.onitsiky@gmail.com>
2023-05-19 09:28:25 +0300

    feat: implement sending email for requesting feedback

--- src/main/java/app/bpartners/api/service/FeedbackRequestedService.java
+++ src/main/java/app/bpartners/api/service/FeedbackRequestedService.java
@@ -1,13 +1,38 @@
 package app.bpartners.api.service;
 
 import app.bpartners.api.endpoint.event.model.gen.FeedbackRequested;
+import app.bpartners.api.model.Attachment;
+import app.bpartners.api.service.aws.SesService;
+import java.io.IOException;
+import java.util.List;
 import java.util.function.Consumer;
+import javax.mail.MessagingException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
+@Slf4j
 public class FeedbackRequestedService implements Consumer<FeedbackRequested> {
+  private final SesService service;
+
   @Override
   public void accept(FeedbackRequested feedbackRequested) {
-    //TODO: send mail ... etc
+    List<String> recipients = feedbackRequested.getRecipientsEmails();
+    String subject = feedbackRequested.getSubject();
+    String htmlBody = feedbackRequested.getMessage();
+    //TODO: configure attachment
+    String attachmentName = feedbackRequested.getAttachmentName();
+    Attachment attachment = Attachment.builder()
+        .name(attachmentName)
+        .build();
+    recipients.forEach((recipient) -> {
+      try {
+        service.sendEmail(recipient, subject, htmlBody, List.of(attachment));
+      } catch (MessagingException | IOException e) {
+        log.error("Email not sent : " + e.getMessage());
+      }
+    });
   }
 }
--- src/main/java/app/bpartners/api/service/FeedbackService.java
+++ src/main/java/app/bpartners/api/service/FeedbackService.java
@@ -4,9 +4,11 @@ import app.bpartners.api.endpoint.event.EventProducer;
 import app.bpartners.api.endpoint.event.model.TypedEvent;
 import app.bpartners.api.endpoint.event.model.TypedFeedbackRequested;
 import app.bpartners.api.endpoint.event.model.gen.FeedbackRequested;
+import app.bpartners.api.model.Customer;
 import app.bpartners.api.model.Feedback;
 import app.bpartners.api.repository.FeedBackRepository;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,16 +19,23 @@ public class FeedbackService {
   private final EventProducer eventProducer;
 
   public Feedback save(String accountHolderId, Feedback toSave) {
-    eventProducer.accept(List.of(toEvent(toSave)));
-
+    eventProducer.accept(List.of(toTypedEvent(toSave)));
     return repository.saveAll(accountHolderId, List.of(toSave)).get(0);
   }
 
-  private TypedEvent toEvent(Feedback feedback) {
+  private TypedEvent toTypedEvent(Feedback feedback) {
     return new TypedFeedbackRequested(FeedbackRequested.builder()
         .subject(feedback.getSubject())
         .message(feedback.getMessage())
-        //TODO
+        .attachmentName(null)
+        .recipientsEmails(getRecipientEmails(feedback.getCustomers()))
         .build());
+  }
+
+  private List<String> getRecipientEmails(List<Customer> recipients) {
+    return recipients.stream()
+        .map(Customer::getEmail)
+        .collect(Collectors.toUnmodifiableList());
+
   }
 }

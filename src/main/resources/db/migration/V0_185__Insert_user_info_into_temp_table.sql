d5d9fc7c7d7e1589d805e4fb484b5ceb541e0b8a
onitsiky <hei.onitsiky@gmail.com>
2023-05-24 10:01:14 +0300

    chore: update sql scripts for getting billing info

---
+++ src/main/resources/db/migration/V0_185__Insert_user_info_into_temp_table.sql
@@ -0,0 +1,14 @@
+drop function insert_account_info_in_temp_table();
+
+create or replace function insert_user_info_into_temp_table()
+    returns void
+as
+$$
+BEGIN
+    insert into annual_billing_info (user_id, first_name, last_name)
+    select distinct pr.id_user, u.first_name, u.last_name
+    from payment_request pr
+             inner join public."user" u on u.id = pr.id_user;
+end ;
+$$
+    language plpgsql;

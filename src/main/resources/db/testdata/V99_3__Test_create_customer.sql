d9ff698866d544a71f7207bf620cb9c78c3187cd
onitsiky <hei.onitsiky@gmail.com>
2023-08-17 16:28:58 +0300

    chore: set customer latitude and longitude in test datas

--- src/main/resources/db/testdata/V99_3__Test_create_customer.sql
+++ src/main/resources/db/testdata/V99_3__Test_create_customer.sql
@@ -1,23 +1,23 @@
 insert into "customer"
 (id, id_user, first_name, last_name, email, phone, website, address, zip_code, city, country,
- comment)
+ comment, latitude, longitude)
 values ('customer1_id', 'joe_doe_id', 'Luc', 'Artisan', 'bpartners.artisans@gmail.com',
         '+33 12 34 56 78', 'https://luc.website.com', '15 rue Porte d''Orange', 95160,
-        'Metz', null, 'Rencontre avec Luc'),
+        'Metz', null, 'Rencontre avec Luc', 0, 0),
        ('customer2_id', 'joe_doe_id', 'Jean',
         'Plombier', null,
         '+33 12 34 56 78', 'https://jean.website.com', '4 Avenue des Près', 95160,
-        'Montmorency', 'France', 'Rencontre avec le plombier'),
+        'Montmorency', 'France', 'Rencontre avec le plombier', 0, 0),
        ('customer3_id', 'joe_doe_id', 'Marc',
         ' Montagnier', 'marc@email' ||
                        '.com',
         '+33 12 34 15 79', 'https://marc.website.com', '4 Avenue des Près', 95160,
-        'Montmorency', 'France', 'Nouvelle rencontre'),
+        'Montmorency', 'France', 'Nouvelle rencontre', 0, 0),
        ('customer4_id', 'joe_doe_id', 'Jean Olivier',
         ' LeBlanc', 'olivier.jean@email' ||
                     '.com',
         '+33 12 34 58 79', 'https://olivier.website.com', '4 Avenue des Près', 95160,
-        'Berlin', 'Allemagne', 'Rencontre à Berlin'),
+        'Berlin', 'Allemagne', 'Rencontre à Berlin', 0, 0),
        ('customer5_id', 'jane_doe_id', 'Frank', 'Dubois', 'frank@email.com',
         '+33 12 34 53 78', 'https://frank.website.com', '5 Avenue des Près', 95160,
-        'Montmorency', 'France', 'Belle rencontre')
+        'Montmorency', 'France', 'Belle rencontre', 0, 0)

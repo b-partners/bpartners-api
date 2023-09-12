b534a4f1a128cc2ef0e264f6c261ff79e9911f16
onitsiky <hei.onitsiky@gmail.com>
2023-09-12 10:15:57 +0300

    chore: add integration tests for calendar event endpoints

--- src/test/java/app/bpartners/api/integration/CalendarEventIT.java
+++ src/test/java/app/bpartners/api/integration/CalendarEventIT.java
@@ -5,13 +5,23 @@ import app.bpartners.api.endpoint.rest.client.ApiClient;
 import app.bpartners.api.endpoint.rest.client.ApiException;
 import app.bpartners.api.endpoint.rest.model.CalendarAuth;
 import app.bpartners.api.endpoint.rest.model.CalendarConsentInit;
+import app.bpartners.api.endpoint.rest.model.CalendarEvent;
+import app.bpartners.api.endpoint.rest.model.CreateCalendarEvent;
 import app.bpartners.api.endpoint.rest.model.Redirection1;
 import app.bpartners.api.endpoint.rest.model.RedirectionStatusUrls;
 import app.bpartners.api.integration.conf.DbEnvContextInitializer;
 import app.bpartners.api.integration.conf.MockedThirdParties;
 import app.bpartners.api.integration.conf.utils.TestUtils;
 import app.bpartners.api.repository.google.calendar.CalendarConf;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -28,6 +38,8 @@ import static app.bpartners.api.integration.conf.utils.TestUtils.setUpCognito;
 import static app.bpartners.api.integration.conf.utils.TestUtils.setUpLegalFileRepository;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -56,6 +68,71 @@ public class CalendarEventIT extends MockedThirdParties {
   private static ApiClient anApiClient() {
     return TestUtils.anApiClient(TestUtils.JOE_DOE_TOKEN,
         DbEnvContextInitializer.getHttpServerPort());
+  }
+
+  static CalendarEvent expectedCalendarEvent() {
+    return new CalendarEvent()
+        .id(null)
+        .summary(null)
+        .location(null)
+        .organizer("dummy")
+        .participants(List.of())
+        .from(null)
+        .to(null)
+        .updatedAt(null);
+
+  }
+
+  static Event expectedCreatedEvent() {
+    Event.Organizer organizer = new Event.Organizer()
+        .setEmail("dummy");
+    EventDateTime dateTime = new EventDateTime()
+        .setDateTime(new DateTime(new Date()))
+        .setTimeZone("America/New_York");
+    return new Event()
+        .setOrganizer(organizer)
+        .setStart(dateTime)
+        .setEnd(dateTime)
+        .setUpdated(new DateTime(new Date()));
+  }
+
+  static Event expectedUpdatedEvent() {
+    List<EventAttendee> attendees = List.of(
+        new EventAttendee().setEmail("joe"),
+        new EventAttendee().setEmail("jane"),
+        new EventAttendee().setEmail("john")
+    );
+    return expectedCreatedEvent()
+        .setSummary("Test update calendar event")
+        .setLocation("Paris, France")
+        .setAttendees(attendees);
+  }
+
+  static CreateCalendarEvent calendarEventToCreate1() {
+    return new CreateCalendarEvent()
+        .summary(null)
+        .location(null)
+        .organizer("dummy")
+        .participants(List.of())
+        .from(Instant.now())
+        .to(Instant.now().plus(1, ChronoUnit.DAYS));
+  }
+
+  static CreateCalendarEvent calendarEventToCreate2() {
+    return new CreateCalendarEvent()
+        .summary(null)
+        .location(null)
+        .organizer("dummy")
+        .participants(List.of())
+        .from(Instant.now())
+        .to(Instant.now().plus(2, ChronoUnit.DAYS));
+  }
+
+  static CreateCalendarEvent calendarEventToUpdate() {
+    return calendarEventToCreate1()
+        .summary("Test update calendar event")
+        .location("Paris, France")
+        .participants(List.of("joe", "jane", "john"));
   }
 
   @BeforeEach
@@ -129,5 +206,54 @@ public class CalendarEventIT extends MockedThirdParties {
         () -> api.exchangeCode(JOE_DOE_ID,
             calendarAuth().redirectUrls(
                 new RedirectionStatusUrls().successUrl("http://localhost:8080/success"))));
+  }
+
+  @Test
+  void get_calendar_events_ok() throws ApiException {
+    ApiClient joeDoeClient = anApiClient();
+    CalendarApi api = new CalendarApi(joeDoeClient);
+    when(calendarApiMock.getEvents(any(), (String) any(), any(), any())).thenReturn(
+        List.of(expectedCreatedEvent()));
+
+    List<CalendarEvent> actual = api.getCalendarEvents(JOE_DOE_ID, "calendar1_id", null);
+
+    assertEquals(List.of(expectedCalendarEvent()), actual.stream()
+        .map(event -> {
+          event.id(null);
+          event.setFrom(null);
+          event.to(null);
+          event.updatedAt(null);
+          return event;
+        }).collect(Collectors.toList()));
+  }
+
+  @Test
+  void crupdate_calendar_events_ok() throws ApiException {
+    ApiClient joeDoeClient = anApiClient();
+    CalendarApi api = new CalendarApi(joeDoeClient);
+    when(calendarApiMock.createEvents((String) any(), any(), any())).thenReturn(
+        List.of(expectedCreatedEvent(), expectedCreatedEvent()),
+        List.of(expectedUpdatedEvent()));
+
+    List<CalendarEvent> savedCalendarEvents = api.crupdateCalendarEvents(JOE_DOE_ID,
+        "calendar1_id", List.of(calendarEventToCreate1(), calendarEventToCreate2()));
+    calendarEventToUpdate().setId(savedCalendarEvents.get(0).getId());
+    List<CalendarEvent> updatedCalendarEvents = api.crupdateCalendarEvents(JOE_DOE_ID,
+        "calendar1_id", List.of(calendarEventToUpdate()));
+
+    assertEquals(2, savedCalendarEvents.size());
+    assertTrue(savedCalendarEvents.stream()
+        .peek(event -> {
+          event.setId(null);
+          event.setFrom(null);
+          event.setTo(null);
+          event.setUpdatedAt(null);
+        })
+        .collect(Collectors.toList())
+        .containsAll(List.of(expectedCalendarEvent(), expectedCalendarEvent())));
+    assertEquals("Test update calendar event", updatedCalendarEvents.get(0).getSummary());
+    assertEquals("Paris, France", updatedCalendarEvents.get(0).getLocation());
+    assertEquals(List.of("joe", "jane", "john"), updatedCalendarEvents.get(0).getParticipants());
+    assertNotNull(updatedCalendarEvents.get(0).getUpdatedAt());
   }
 }

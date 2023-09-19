e73875f9a5d266f21f1dac814d91a1e56439f09d
Ryan Andriamahery <ryan@hei.school>
2023-09-19 13:48:16 +0300

    chore: add todos for CalendarEventIT

--- src/test/java/app/bpartners/api/integration/CalendarEventIT.java
+++ src/test/java/app/bpartners/api/integration/CalendarEventIT.java
@@ -176,6 +176,7 @@ public class CalendarEventIT extends MockedThirdParties {
                 new RedirectionStatusUrls().successUrl("http://localhost:8080/success"))));
   }
 
+  //TODO: make test pass
   @Test
   void handle_auth_ok() {
     ApiClient joeDoeClient = anApiClient();
@@ -208,6 +209,7 @@ public class CalendarEventIT extends MockedThirdParties {
                 new RedirectionStatusUrls().successUrl("http://localhost:8080/success"))));
   }
 
+  //TODO: make test pass
   @Test
   void get_calendar_events_ok() throws ApiException {
     ApiClient joeDoeClient = anApiClient();
@@ -227,6 +229,7 @@ public class CalendarEventIT extends MockedThirdParties {
         }).collect(Collectors.toList()));
   }
 
+  //TODO: make test pass
   @Test
   void crupdate_calendar_events_ok() throws ApiException {
     ApiClient joeDoeClient = anApiClient();

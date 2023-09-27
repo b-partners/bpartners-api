1fd069861cdd6a87e9d1d29a3cf285dbd38b1779
Ryan Andriamahery <ryan@hei.school>
2023-09-27 14:11:01 +0300

    chore: handle spreadsheet evaluation for prospect evaluation and add job rest validator

--- src/main/java/app/bpartners/api/model/prospect/job/ProspectEvaluationJobRunner.java
+++ src/main/java/app/bpartners/api/model/prospect/job/ProspectEvaluationJobRunner.java
@@ -17,8 +17,13 @@ import lombok.ToString;
 public class ProspectEvaluationJobRunner implements Serializable {
   private String jobId;
   private EventJobRunner eventJobRunner;
+  private SheetEvaluationJobRunner sheetProspectEvaluation;
 
   public boolean isEventConversionJob() {
     return eventJobRunner != null;
+  }
+
+  public boolean isSpreadsheetEvaluationJob() {
+    return sheetProspectEvaluation != null;
   }
 }

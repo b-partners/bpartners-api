e5b7b508ef4ae2c7193a9e8f0608f2770bf727b8
Ryan Andriamahery <ryan@hei.school>
2023-11-30 12:21:05 +0300

    fix: send email when prospect is given up

--- src/main/java/app/bpartners/api/service/aws/ProspectUpdatedService.java
+++ src/main/java/app/bpartners/api/service/aws/ProspectUpdatedService.java
@@ -35,15 +35,26 @@ public class ProspectUpdatedService implements Consumer<ProspectUpdated> {
   @Override
   public void accept(ProspectUpdated prospectUpdated) {
     Prospect prospect = prospectUpdated.getProspect();
+    boolean prospectGivenUp =
+        prospect.getIdHolderOwner() == null && prospect.getLatestOldHolder() != null;
+    AccountHolder accountHolder =
+        prospectGivenUp ? holderRepository.findById(prospect.getLatestOldHolder())
+            : holderRepository.findById(prospect.getIdHolderOwner());
+    ProspectUpdateType updateType = prospectGivenUp ? ProspectUpdateType.GIVE_UP
+        : ProspectUpdateType.CONTINUE_PROCESS;
     Instant updatedAt = prospectUpdated.getUpdatedAt();
-    AccountHolder accountHolder = holderRepository.findById(prospect.getIdHolderOwner());
     try {
       String recipient = eventConf.getAdminEmail();
       String concerned = null;
       String frenchUpdatedDatetime = formatFrenchDatetime(updatedAt);
       String translatedStatus = getTranslatedStatus(prospect.getActualStatus());
       String translatedFeedback = getTranslatedFeedBack(prospect.getProspectFeedback());
-      String subject = String.format(
+      String subject = prospectGivenUp ? String.format(
+          "Le prospect intitulé %s a été abandonné par l'artisan %s le %s",
+          prospect.getName(),
+          accountHolder.getName(),
+          frenchUpdatedDatetime)
+          : String.format(
           "Le prospect intitulé %s appartenant à l'artisan %s est passé en statut %s le %s",
           prospect.getName(),
           accountHolder.getName(),
@@ -54,7 +65,8 @@ public class ProspectUpdatedService implements Consumer<ProspectUpdated> {
           accountHolder,
           translatedStatus,
           frenchUpdatedDatetime,
-          translatedFeedback);
+          translatedFeedback,
+          updateType);
       List<Attachment> attachments = List.of();
       sesService.sendEmail(recipient, concerned, subject, htmlBody, attachments);
       log.info("{} updated and mail sent to recipient={}", prospect.describe(), recipient);
@@ -92,8 +104,10 @@ public class ProspectUpdatedService implements Consumer<ProspectUpdated> {
                           AccountHolder accountHolder,
                           String translatedStatus,
                           String frenchUpdatedDatetime,
-                          String translatedFeedback) {
+                          String translatedFeedback,
+                          ProspectUpdateType updateType) {
     Context context = new Context();
+    context.setVariable("updateType", updateType);
     context.setVariable("prospect", prospect);
     context.setVariable("translatedFeedback", translatedFeedback);
     context.setVariable("accountHolder", accountHolder);
@@ -101,5 +115,9 @@ public class ProspectUpdatedService implements Consumer<ProspectUpdated> {
     context.setVariable("frenchUpdatedDatetime", frenchUpdatedDatetime);
     return TemplateResolverUtils.parseTemplateResolver(
         PROSPECT_UPDATED_TEMPLATE, context);
+  }
+
+  enum ProspectUpdateType {
+    GIVE_UP, CONTINUE_PROCESS
   }
 }
---
+++ src/main/resources/db/migration/V0_241__Add_latest_holder_owner_to_prospect.sql
@@ -0,0 +1,2 @@
+alter table "prospect"
+    add column if not exists "latest_old_holder" varchar;

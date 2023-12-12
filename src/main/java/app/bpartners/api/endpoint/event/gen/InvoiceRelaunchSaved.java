3340a562026e30184dbe6784be7d30d287396a98
Onitsiky <hei.onitsiky@gmail.com>
2023-12-12 16:06:05 +0300

    poja: ZE GREAT MIGRATION HAPPENED

--- src/main/java/app/bpartners/api/endpoint/event/gen/InvoiceRelaunchSaved.java
+++ src/main/java/app/bpartners/api/endpoint/event/gen/InvoiceRelaunchSaved.java
@@ -1,26 +1,37 @@
-package app.bpartners.api.endpoint.event.model.gen;
+package app.bpartners.api.endpoint.event.gen;
 
 import app.bpartners.api.model.AccountHolder;
+import app.bpartners.api.model.Attachment;
 import app.bpartners.api.model.Invoice;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.List;
 import javax.annotation.processing.Generated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Generated("EventBridge")
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Data
 @ToString
-public class InvoiceCrupdated implements Serializable {
+@EqualsAndHashCode
+public class InvoiceRelaunchSaved implements Serializable {
   private static final long serialVersionUID = 1L;
 
+  @JsonProperty("recipient")
+  private String recipient = null;
+  @JsonProperty("subject")
+  private String subject = null;
+  @JsonProperty("htmlBody")
+  private String htmlBody = null;
+  @JsonProperty("attachmentName")
+  private String attachmentName;
   @JsonProperty("invoice")
   private Invoice invoice;
   @JsonProperty("accountHolder")
@@ -28,20 +39,6 @@ public class InvoiceCrupdated implements Serializable {
   @JsonProperty("logoFileId")
   private String logoFileId;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    InvoiceCrupdated invoiceCrupdated = (InvoiceCrupdated) o;
-    return Objects.equals(this.invoice, invoiceCrupdated.invoice);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(invoice);
-  }
+  @JsonProperty("attachments")
+  private List<Attachment> attachments;
 }

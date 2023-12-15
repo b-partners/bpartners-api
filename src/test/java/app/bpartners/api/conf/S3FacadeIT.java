a9ae36aee2e79b0652a308efbb53672ea3ba5e94
Numer-Mobile-2 <numer@hei.school>
2023-12-15 14:09:56 +0300

    chore(tests): use S3FacadeIT instead of S3AbstractContextInitializer

--- src/test/java/app/bpartners/api/conf/S3FacadeIT.java
+++ src/test/java/app/bpartners/api/conf/S3FacadeIT.java
@@ -1,2 +1,51 @@
-package app.bpartners.api.conf;public class S3FacadeIT {
+package app.bpartners.api.conf;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import static java.lang.Runtime.getRuntime;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Slf4j
+public class S3FacadeIT {
+    private static final PostgresConf POSTGRES_CONF = new PostgresConf();
+    private static final LocalStackConf LOCAL_STACK_CONF = new LocalStackConf();
+    private static final EventConf EVENT_CONF = new EventConf();
+
+    @BeforeAll
+    static void beforeAll() {
+        POSTGRES_CONF.start();
+        LOCAL_STACK_CONF.start();
+        getRuntime()
+                // Do _not_ stop postgresTest in afterAll as it is shared between multiple subclasses of
+                // FacadeTest.
+                // Doing so might cause some subclasses to stop it while other ones are still using it!
+                .addShutdownHook(new Thread(() -> {
+                    LOCAL_STACK_CONF.stop();
+                    POSTGRES_CONF.stop();
+                }));
+    }
+
+    @SneakyThrows
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        POSTGRES_CONF.configureProperties(registry);
+        EVENT_CONF.configureProperties(registry);
+        LOCAL_STACK_CONF.configureProperties(registry);
+
+        try {
+            var envConfClazz = Class.forName("app.bpartners.api.conf.EnvConf");
+            var envConfConfigureProperties =
+                    envConfClazz.getDeclaredMethod("configureProperties", DynamicPropertyRegistry.class);
+            var envConf = envConfClazz.getConstructor().newInstance();
+            envConfConfigureProperties.invoke(envConf, registry);
+        } catch (ClassNotFoundException e) {
+            log.warn("EnvConf missing: no project-specific test env vars will be set");
+        }
+    }
 }
--- src/test/java/app/bpartners/api/integration/InvoiceRelaunchConfIT.java
+++ src/test/java/app/bpartners/api/integration/InvoiceRelaunchConfIT.java
@@ -6,6 +6,7 @@ import app.bpartners.api.endpoint.rest.client.ApiException;
 import app.bpartners.api.endpoint.rest.model.InvoiceRelaunchConf;
 import app.bpartners.api.integration.conf.MockedThirdParties;
 import app.bpartners.api.integration.conf.S3AbstractContextInitializer;
+import app.bpartners.api.integration.conf.S3MockedThirdParties;
 import app.bpartners.api.integration.conf.utils.TestUtils;
 import app.bpartners.api.repository.fintecture.FintecturePaymentInfoRepository;
 import app.bpartners.api.repository.fintecture.FintecturePaymentInitiationRepository;
@@ -20,32 +21,22 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static app.bpartners.api.integration.conf.utils.TestUtils.INVOICE1_ID;
-import static app.bpartners.api.integration.conf.utils.TestUtils.INVOICE2_ID;
-import static app.bpartners.api.integration.conf.utils.TestUtils.JOE_DOE_ACCOUNT_ID;
-import static app.bpartners.api.integration.conf.utils.TestUtils.setUpCognito;
-import static app.bpartners.api.integration.conf.utils.TestUtils.setUpLegalFileRepository;
-import static app.bpartners.api.integration.conf.utils.TestUtils.setUpPaymentInfoRepository;
-import static app.bpartners.api.integration.conf.utils.TestUtils.setUpPaymentInitiationRep;
-import static app.bpartners.api.integration.conf.utils.TestUtils.setUpS3Conf;
+import static app.bpartners.api.integration.conf.utils.TestUtils.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
 @Testcontainers
-@ContextConfiguration(initializers = InvoiceRelaunchConfIT.ContextInitializer.class)
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class InvoiceRelaunchConfIT extends MockedThirdParties {
+class InvoiceRelaunchConfIT extends S3MockedThirdParties {
   private static final String RELAUNCH_CONF1_ID = "relaunchConf1_id";
   @MockBean
   private FintecturePaymentInitiationRepository paymentInitiationRepositoryMock;
   @MockBean
   private FintecturePaymentInfoRepository paymentInfoRepositoryMock;
 
-  private static ApiClient anApiClient() {
-    return TestUtils.anApiClient(TestUtils.JOE_DOE_TOKEN,
-        InvoiceRelaunchConfIT.ContextInitializer.SERVER_PORT);
+  private ApiClient anApiClient() {
+    return TestUtils.anApiClient(TestUtils.JOE_DOE_TOKEN, localPort);
   }
 
   @BeforeEach
@@ -54,7 +45,6 @@ class InvoiceRelaunchConfIT extends MockedThirdParties {
     setUpPaymentInfoRepository(paymentInfoRepositoryMock);
     setUpLegalFileRepository(legalFileRepositoryMock);
     setUpCognito(cognitoComponentMock);
-    setUpS3Conf(s3Conf);
   }
 
   @Test
@@ -107,14 +97,5 @@ class InvoiceRelaunchConfIT extends MockedThirdParties {
     return createInvoiceRelaunchConf()
         .delay(10)
         .rehearsalNumber(15);
-  }
-
-  static class ContextInitializer extends S3AbstractContextInitializer {
-    public static final int SERVER_PORT = TestUtils.findAvailableTcpPort();
-
-    @Override
-    public int getServerPort() {
-      return SERVER_PORT;
-    }
   }
 }

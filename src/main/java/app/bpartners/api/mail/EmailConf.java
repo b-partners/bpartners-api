b48fb90c89952ecde98470ed5f36541fd5eef6dd
Onitsiky <hei.onitsiky@gmail.com>
2024-01-03 13:57:02 +0300

    chore: upgrade poja to v9.1.0

--- src/main/java/app/bpartners/api/mail/EmailConf.java
+++ src/main/java/app/bpartners/api/mail/EmailConf.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ses.SesClient;
 
 @PojaGenerated
@@ -12,13 +13,16 @@ import software.amazon.awssdk.services.ses.SesClient;
 public class EmailConf {
 
   @Getter private final String sesSource;
+  private final Region region;
 
-  public EmailConf(@Value("${aws.ses.source}") String sesSource) {
+  public EmailConf(
+      @Value("${aws.ses.source}") String sesSource, @Value("eu-west-3") Region region) {
     this.sesSource = sesSource;
+    this.region = region;
   }
 
   @Bean
   public SesClient getSesClient() {
-    return SesClient.create();
+    return SesClient.builder().region(region).build();
   }
 }

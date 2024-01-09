8ac7ad2e5864db9ca821b4d8d8602393bd300880
Onitsiky <hei.onitsiky@gmail.com>
2024-01-09 07:38:46 +0300

    poja-upgrade: 10.3.0

--- src/main/java/app/bpartners/api/endpoint/EndpointConf.java
+++ src/main/java/app/bpartners/api/endpoint/EndpointConf.java
@@ -1,6 +1,7 @@
 package app.bpartners.api.endpoint;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,10 @@ import org.springframework.context.annotation.Configuration;
 public class EndpointConf {
   @Bean
   public ObjectMapper objectMapper() {
-    return new ObjectMapper().configure(FAIL_ON_UNKNOWN_PROPERTIES, false).findAndRegisterModules();
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
+    objectMapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
+    objectMapper.findAndRegisterModules();
+    return objectMapper;
   }
 }

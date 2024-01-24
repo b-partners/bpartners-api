9031335e390d4922530cffdd762b625d0885b60e
Onitsiky <hei.onitsiky@gmail.com>
2024-01-24 18:56:41 +0300

    poja-upgrade: 10.5.1 (#1198)

--- src/main/java/app/bpartners/api/endpoint/EndpointConf.java
+++ src/main/java/app/bpartners/api/endpoint/EndpointConf.java
@@ -3,10 +3,12 @@ package app.bpartners.api.endpoint;
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
+import app.bpartners.api.PojaGenerated;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@PojaGenerated
 @Configuration
 public class EndpointConf {
   @Bean

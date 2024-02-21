117b2bf154eaeb0cc40e7785b1fcf0f467446598
Mahefaa <hei.mahefa@gmail.com>
2024-02-21 14:50:23 +0300

    feat: multipart upload

--- src/main/java/app/bpartners/api/endpoint/rest/controller/FileController.java
+++ src/main/java/app/bpartners/api/endpoint/rest/controller/FileController.java
@@ -1,8 +1,11 @@
 package app.bpartners.api.endpoint.rest.controller;
 
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
 import app.bpartners.api.endpoint.rest.model.FileInfo;
 import app.bpartners.api.endpoint.rest.model.FileType;
 import app.bpartners.api.endpoint.rest.security.AuthProvider;
+import app.bpartners.api.file.MultipartFileConverter;
 import app.bpartners.api.model.mapper.FileMapper;
 import app.bpartners.api.service.FileService;
 import app.bpartners.api.service.utils.FileInfoUtils;
@@ -14,14 +17,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @AllArgsConstructor
 @Slf4j
 public class FileController {
-  private FileService service;
-  private FileMapper mapper;
+  private final FileService service;
+  private final FileMapper mapper;
+  private final MultipartFileConverter multipartFileConverter;
 
   @GetMapping(value = "/accounts/{accountId}/files/{id}")
   public FileInfo getFileInfoById(
@@ -55,5 +61,20 @@ public class FileController {
     return ResponseEntity.ok()
         .contentType(FileInfoUtils.parseMediaTypeFromBytes(toUpload))
         .body(toUpload);
+  }
+
+  @PostMapping(value = "/accounts/{accountId}/files/{id}/multipart", consumes = MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<byte[]> uploadMultipartFile(
+          @PathVariable(name = "accountId") String accountId,
+          @PathVariable(name = "id") String fileId,
+          @RequestParam(name = "fileType") FileType fileType,
+          @RequestPart(name = "file") MultipartFile file) {
+    var toUpload = multipartFileConverter.apply(file);
+    String idUser =
+            AuthProvider.getAuthenticatedUserId(); // TODO: should be changed when endpoint changed
+    service.upload(fileId, fileType, idUser, toUpload);
+    return ResponseEntity.ok()
+            .contentType(FileInfoUtils.parseMediaTypeFromBytes(toUpload))
+            .body(toUpload);
   }
 }
---
+++ src/main/java/app/bpartners/api/file/MultipartFileConverter.java
@@ -0,0 +1,19 @@
+package app.bpartners.api.file;
+
+import app.bpartners.api.model.exception.ApiException;
+import java.io.IOException;
+import java.util.function.Function;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class MultipartFileConverter implements Function<MultipartFile, byte[]> {
+    @Override
+    public byte[] apply(MultipartFile file) {
+        try {
+            return file.getBytes();
+        } catch (IOException e) {
+            throw new ApiException(ApiException.ExceptionType.SERVER_EXCEPTION, e);
+        }
+    }
+}

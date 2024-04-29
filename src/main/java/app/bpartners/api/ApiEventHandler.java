4c7b362a9201b8c6dda6b0c2c7001b252a2139d7
dinasoa <hei.dinasoa@gmail.com>
2024-04-29 15:02:33 +0300

    chore: refresh database connection before snapshot

--- src/main/java/app/bpartners/api/ApiEventHandler.java
+++ src/main/java/app/bpartners/api/ApiEventHandler.java
@@ -9,10 +9,24 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.crac.Core;
+import org.crac.Resource;
 
 @PojaGenerated
-public class ApiEventHandler implements RequestStreamHandler {
+public class ApiEventHandler implements RequestStreamHandler, Resource {
   private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+  private final DataSource dataSource;
+  private static final Logger logger = Logger.getLogger(ApiEventHandler.class.getName());
+
+  public ApiEventHandler(DataSource dataSource) {
+    Core.getGlobalContext().register(this);
+    this.dataSource = dataSource;
+  }
 
   static {
     try {
@@ -26,5 +40,25 @@ public class ApiEventHandler implements RequestStreamHandler {
   public void handleRequest(InputStream input, OutputStream output, Context context)
       throws IOException {
     handler.proxyStream(input, output, context);
+  }
+
+  @Override
+  public void beforeCheckpoint(org.crac.Context<? extends Resource> context) throws Exception {
+    refreshDatabaseConnection();
+  }
+
+  @Override
+  public void afterRestore(org.crac.Context<? extends Resource> context) throws Exception {
+    refreshDatabaseConnection();
+  }
+
+  private void refreshDatabaseConnection() {
+    try {
+      Connection connection = dataSource.getConnection();
+      connection.close();
+      logger.info("Database connection refreshed successfully.");
+    } catch (SQLException e) {
+      logger.log(Level.SEVERE, "Error refreshing database connection", e);
+    }
   }
 }

269ebfaa366fcd72dbe5c3f74a083bd207045139
Mahefa Ny Anjara <98638690+Mahefaa@users.noreply.github.com>
2024-06-05 09:14:41 +0300

    chore: cascade retry WmsImageSource if image is blank or ApiException is thrown
    * add geoserver IGN as primary default server
    * OSM tous_fr now serves as the secondary default server

---
+++ src/main/resources/db/migration/V43_22__Insert_area_picture_map_layer_default_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO area_picture_map_layer (id, source, year, name, departement_name, maximum_zoom_level, precision_level_in_cm)
+VALUES ('9a4bd8b7-556b-49a1-bea0-c35e961dab64', 'GEOSERVER', 2023, 'FLUX_IGN_2023_20CM', 'ALL', 'HOUSES_0', 20);

09fa330ee5c0cd14b73ef0272e71d36b844e31cc
Mahefaa <hei.mahefa@gmail.com>
2024-06-10 14:39:59 +0300

    chore: configure RestTemplate error handling

--- src/main/java/app/bpartners/api/PojaApplication.java
+++ src/main/java/app/bpartners/api/PojaApplication.java
@@ -2,20 +2,11 @@ package app.bpartners.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @PojaGenerated
 public class PojaApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(PojaApplication.class, args);
-  }
-
-  @Bean
-  public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-    return restTemplateBuilder.build();
   }
 }

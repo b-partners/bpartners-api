77a10ff87c7387b1f5f9af9f1371c42c13d1c842
nirinaHelena <hei.nirina.7@gmail.com>
2024-08-23 12:01:49 +0300

    test: UserServiceTest::change_active_account_ok

--- src/test/java/app/bpartners/api/unit/service/UserServiceTest.java
+++ src/test/java/app/bpartners/api/unit/service/UserServiceTest.java
@@ -124,13 +124,29 @@ class UserServiceTest {
     when(user.getAccounts()).thenReturn(accounts);
     when(accounts.get(anyInt())).thenReturn(defaultAccount);
     when(defaultAccount.getId()).thenReturn("default_account_id");
-    when(userRepository.save(any())).thenReturn(user());
 
     assertThrows(
         NotFoundException.class,
         () -> {
           userService.changeActiveAccount(JOE_DOE_USER_ID, JANE_ACCOUNT_ID);
         });
+  }
+
+  @Test
+  void change_active_account_ok() {
+    var user = mock(User.class);
+    var defaultAccount = mock(Account.class);
+    var accounts = mock(List.class);
+
+    when(userRepository.getById(any())).thenReturn(user);
+    when(user.getDefaultAccount()).thenReturn(defaultAccount);
+    when(defaultAccount.getId()).thenReturn(JOE_DOE_ACCOUNT_ID);
+    when(user.getAccounts()).thenReturn(accounts);
+    when(accounts.get(anyInt())).thenReturn(defaultAccount);
+    when(userRepository.save(any())).thenReturn(user());
+
+    var actual = userService.changeActiveAccount(USER1_ID, JOE_DOE_ACCOUNT_ID);
+    assertEquals(user, actual);
   }
 
   @Test

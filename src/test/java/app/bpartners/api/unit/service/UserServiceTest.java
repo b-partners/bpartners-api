2fa9b75bc9c5fb6bd84d04cffd7c7110a5b35f82
nirinaHelena <hei.nirina.7@gmail.com>
2024-08-23 11:38:05 +0300

    test: UserServiceTest::change_active_account_not_found_exception

--- src/test/java/app/bpartners/api/unit/service/UserServiceTest.java
+++ src/test/java/app/bpartners/api/unit/service/UserServiceTest.java
@@ -1,19 +1,21 @@
 package app.bpartners.api.unit.service;
 
 import static app.bpartners.api.integration.conf.utils.TestUtils.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import app.bpartners.api.model.Account;
 import app.bpartners.api.model.User;
 import app.bpartners.api.model.UserToken;
+import app.bpartners.api.model.exception.NotFoundException;
 import app.bpartners.api.repository.UserRepository;
 import app.bpartners.api.repository.UserTokenRepository;
 import app.bpartners.api.service.SnsService;
 import app.bpartners.api.service.UserService;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -70,6 +72,26 @@ class UserServiceTest {
 
     var actual = userService.changeActiveAccount(JOE_DOE_ID, JOE_DOE_ACCOUNT_ID);
     assertEquals(user, actual);
+  }
+
+  @Test
+  void change_active_account_not_found_exception() {
+    var user = mock(User.class);
+    var defaultAccount = mock(Account.class);
+    var accounts = mock(List.class);
+
+    when(userRepository.getById(any())).thenReturn(user);
+    when(user.getDefaultAccount()).thenReturn(defaultAccount);
+    when(user.getAccounts()).thenReturn(accounts);
+    when(accounts.get(anyInt())).thenReturn(defaultAccount);
+    when(defaultAccount.getId()).thenReturn("default_account_id");
+    when(userRepository.save(any())).thenReturn(user());
+
+    assertThrows(
+        NotFoundException.class,
+        () -> {
+          userService.changeActiveAccount(JOE_DOE_USER_ID, JANE_ACCOUNT_ID);
+        });
   }
 
   @Test

5dedcc9761bf58ad9f2fa2ce8e4ba91b0a268ca9
nirinaHelena <hei.nirina.7@gmail.com>
2024-08-25 22:40:31 +0300

    test: WhoisServiceTest::validateApiKey

--- src/test/java/app/bpartners/api/unit/service/WhoisServiceTest.java
+++ src/test/java/app/bpartners/api/unit/service/WhoisServiceTest.java
@@ -2,13 +2,16 @@ package app.bpartners.api.unit.service;
 
 import app.bpartners.api.model.IntegratingApplication;
 import app.bpartners.api.model.User;
+import app.bpartners.api.model.exception.ForbiddenException;
 import app.bpartners.api.service.UserService;
 import app.bpartners.api.service.WhoisService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import static app.bpartners.api.integration.conf.utils.TestUtils.USER1_ID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -21,6 +24,8 @@ class WhoisServiceTest {
     void setUp() {
         userServiceMock = mock(UserService.class);
         subject = new WhoisService(userServiceMock);
+        ReflectionTestUtils.setField(subject, "FEATURE_DETECTOR_API_KEY", "valid-api-key");
+        ReflectionTestUtils.setField(subject, "FEATURE_DETECTOR_APPLICATION", "test-application");
     }
 
     @Test
@@ -34,5 +39,28 @@ class WhoisServiceTest {
         when(user.getId()).thenReturn(USER1_ID);
 
         assertEquals(user, subject.getSpecifiedUser(application, USER1_ID));
+    }
+
+    @Test
+    void validateApiKeyShouldReturnIntegratingApplicationWhenApiKeyIsValid() {
+        var application = subject.validateApiKey("valid-api-key");
+
+        assertEquals("test-application", application.getApplicationName());
+        assertEquals("valid-api-key", application.getApiKey());
+    }
+
+    @Test
+    void validateApiKeyShouldThrowForbiddenExceptionWhenApiKeyIsInvalid() {
+        assertThrows(ForbiddenException.class, () -> subject.validateApiKey("invalid-api-key"));
+    }
+
+    @Test
+    void validateApiKeyShouldThrowForbiddenExceptionWhenApiKeyIsNull() {
+        assertThrows(ForbiddenException.class, () -> subject.validateApiKey(null));
+    }
+
+    @Test
+    void validateApiKeyShouldThrowForbiddenExceptionWhenApiKeyIsEmpty() {
+        assertThrows(ForbiddenException.class, () -> subject.validateApiKey(""));
     }
 }

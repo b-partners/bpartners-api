da304432650094eb0ee8be9d3e238b8d783587b5
nirinaHelena <hei.nirina.7@gmail.com>
2024-08-25 18:57:16 +0300

    test: AccountHolderServiceTest::update_company_info_ok_with_company_info_not_null

--- src/test/java/app/bpartners/api/unit/service/AccountHolderServiceTest.java
+++ src/test/java/app/bpartners/api/unit/service/AccountHolderServiceTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static app.bpartners.api.integration.conf.utils.TestUtils.ACCOUNTHOLDER_ID;
+import static app.bpartners.api.integration.conf.utils.TestUtils.location;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -31,12 +31,44 @@ class AccountHolderServiceTest {
     }
 
     @Test
-    void update_comany_info_ok_with_company_info_null() {
+    void update_company_info_ok_with_company_info_null() {
         var accountHolder = new AccountHolder();
 
         when(repositoryMock.findById(any())).thenReturn(accountHolder);
         when(repositoryMock.save(any())).thenReturn(accountHolder);
 
         assertEquals(accountHolder, subject.updateCompanyInfo(ACCOUNTHOLDER_ID, null));
+    }
+
+    CompanyInfo companyInfo(){
+        return CompanyInfo.builder()
+                .socialCapital(4000)
+                .email("email@example.com")
+                .website("website example")
+                .phone("+33 12 34 56 78")
+                .subjectToVat(true)
+                .location(location())
+                .townCode(92002)
+                .tvaNumber("FR12323456789")
+                .build();
+    }
+
+    @Test
+    void update_company_info_ok_with_company_info_not_null() {
+        var accountHolder = AccountHolder.builder()
+                .socialCapital(4000)
+                .email("email@example.com")
+                .website("website example")
+                .mobilePhoneNumber("+33 12 34 56 78")
+                .subjectToVat(true)
+                .location(location())
+                .townCode(92002)
+                .vatNumber("FR12323456789")
+                .build();
+
+        when(repositoryMock.findById(any())).thenReturn(accountHolder);
+        when(repositoryMock.save(any())).thenReturn(accountHolder);
+
+        assertEquals(accountHolder, subject.updateCompanyInfo(ACCOUNTHOLDER_ID, companyInfo()));
     }
 }

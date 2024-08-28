746b5e36947590ccf4fe202d6a3343323df9b36c
Dinasoa <hei.dinasoa@gmail.com>
2024-08-28 14:31:39 +0200

    chore: send email when wms image download throws exception and rm osm image source
    # Conflicts:
    #	src/main/java/app/bpartners/api/service/AreaPictureService.java
    #	src/main/java/app/bpartners/api/service/WMS/imageSource/WmsImageSourceFacade.java
    #	src/test/java/app/bpartners/api/integration/wms/IGNGeoserverImageSourceIT.java
    #	src/test/java/app/bpartners/api/service/WMS/imageSource/WmsImageSourceFacadeIT.java

--- src/main/java/app/bpartners/api/service/WMS/imageSource/WmsImageSource.java
+++ src/main/java/app/bpartners/api/service/WMS/imageSource/WmsImageSource.java
@@ -1,9 +1,12 @@
 package app.bpartners.api.service.WMS.imageSource;
 
+import app.bpartners.api.model.AccountHolder;
 import app.bpartners.api.model.AreaPicture;
 import java.io.File;
 
 public interface WmsImageSource {
+  File downloadImage(AreaPicture areaPicture, AccountHolder accountHolder);
+
   File downloadImage(AreaPicture areaPicture);
 
   boolean supports(AreaPicture areaPicture);

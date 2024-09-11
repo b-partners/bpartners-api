3926fda4a96cda6a2ab3ba3b4e1a46de89441278
nirinaHelena <hei.nirina.7@gmail.com>
2024-09-11 21:22:19 +0300

    test: PreUserValidatorTest

--- src/test/java/app/bpartners/api/unit/validator/InvoiceRelaunchValidatorTest.java
+++ src/test/java/app/bpartners/api/unit/validator/InvoiceRelaunchValidatorTest.java
@@ -1,30 +1,34 @@
 package app.bpartners.api.unit.validator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import app.bpartners.api.endpoint.rest.model.InvoiceStatus;
 import app.bpartners.api.model.Invoice;
 import app.bpartners.api.model.exception.BadRequestException;
 import app.bpartners.api.model.validator.InvoiceRelaunchValidator;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 class InvoiceRelaunchValidatorTest {
-    InvoiceRelaunchValidator subject = new InvoiceRelaunchValidator();
+  InvoiceRelaunchValidator subject = new InvoiceRelaunchValidator();
 
-    @Test
-    void subject_throws_bad_request_exception() {
-        var invoice = Invoice.builder().id("id").status(InvoiceStatus.DRAFT).build();
+  @Test
+  void subject_throws_bad_request_exception() {
+    var invoice = Invoice.builder().id("id").status(InvoiceStatus.DRAFT).build();
 
-        var actual = assertThrows(BadRequestException.class, () -> {
-            subject.accept(invoice);
-        });
+    var actual =
+        assertThrows(
+            BadRequestException.class,
+            () -> {
+              subject.accept(invoice);
+            });
 
-        var expected = "Invoice."
-                + invoice.getId()
-                + " actual status is "
-                + invoice.getStatus()
-                + " and it cannot be relaunched";
-        assertEquals(expected, actual.getMessage());
-    }
+    var expected =
+        "Invoice."
+            + invoice.getId()
+            + " actual status is "
+            + invoice.getStatus()
+            + " and it cannot be relaunched";
+    assertEquals(expected, actual.getMessage());
+  }
 }
--- src/test/java/app/bpartners/api/unit/validator/PreUserValidatorTest.java
+++ src/test/java/app/bpartners/api/unit/validator/PreUserValidatorTest.java
@@ -3,26 +3,26 @@ package app.bpartners.api.unit.validator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import app.bpartners.api.model.AreaPictureAnnotationInstance;
+import app.bpartners.api.model.PreUser;
 import app.bpartners.api.model.exception.BadRequestException;
-import app.bpartners.api.model.validator.PolygonValidator;
+import app.bpartners.api.model.validator.PreUserValidator;
 import org.junit.jupiter.api.Test;
 
-class PolygonValidatorTest {
-  PolygonValidator subject = new PolygonValidator();
+class PreUserValidatorTest {
+  PreUserValidator subject = new PreUserValidator();
 
   @Test
   void subject_throws_bad_request_exception() {
-    var polygon = AreaPictureAnnotationInstance.Polygon.builder().build();
+    var preUser = PreUser.builder().email("").build();
 
     var actual =
         assertThrows(
             BadRequestException.class,
             () -> {
-              subject.accept(polygon);
+              subject.accept(preUser);
             });
 
-    var expected = "polygon points attribute is mandatory";
+    var expected = "Invalid email. ";
     assertEquals(expected, actual.getMessage());
   }
 }

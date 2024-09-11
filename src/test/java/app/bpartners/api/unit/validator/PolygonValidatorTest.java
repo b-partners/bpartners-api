5cef7b1b311900a4a66655a480ce4f487f9f4126
nirinaHelena <hei.nirina.7@gmail.com>
2024-09-11 20:58:26 +0300

    test: PolygonValidatorTest

---
+++ src/test/java/app/bpartners/api/unit/validator/PolygonValidatorTest.java
@@ -0,0 +1,25 @@
+package app.bpartners.api.unit.validator;
+
+import app.bpartners.api.model.AreaPictureAnnotationInstance;
+import app.bpartners.api.model.exception.BadRequestException;
+import app.bpartners.api.model.validator.PolygonValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PolygonValidatorTest {
+    PolygonValidator subject = new PolygonValidator();
+
+    @Test
+    void subject_throws_bad_request_exception() {
+        var polygon = AreaPictureAnnotationInstance.Polygon.builder().build();
+
+        var actual = assertThrows(BadRequestException.class, () -> {
+            subject.accept(polygon);
+        });
+
+        var expected = "polygon points attribute is mandatory";
+        assertEquals(expected, actual.getMessage());
+    }
+}

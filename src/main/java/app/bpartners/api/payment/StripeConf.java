03a8207fdb38711075b9b02efe03873dce0b01dc
Ryan Andriamahery <ryan@hei.school>
2024-11-13 13:54:45 +0300

    chore: set StripeConf for StripeClient

---
+++ src/main/java/app/bpartners/api/payment/StripeConf.java
@@ -0,0 +1,24 @@
+package app.bpartners.api.payment;
+
+import com.stripe.StripeClient;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class StripeConf {
+    private final String apiKey;
+
+    public StripeConf(@Value("${stripe.private.api.key}") String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Bean
+    public StripeClient stripeClient() {
+        return StripeClient.builder()
+                .setApiKey(apiKey)
+                .build();
+    }
+}

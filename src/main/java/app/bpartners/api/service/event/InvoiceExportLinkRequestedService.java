df13d29f7db143791a86bb15264c5cb91378f413
nirinaHelena <hei.nirina.7@gmail.com>
2024-11-27 15:25:02 +0300

    chore: remove log

--- src/main/java/app/bpartners/api/service/event/InvoiceExportLinkRequestedService.java
+++ src/main/java/app/bpartners/api/service/event/InvoiceExportLinkRequestedService.java
@@ -114,7 +114,6 @@ public class InvoiceExportLinkRequestedService implements Consumer<InvoiceExport
         .map(
             invoice -> {
               File file = s3Service.downloadFile(INVOICE, invoice.getFileId(), userId);
-              log.info("download file: {}", file);
               try {
                 Path destinationPath =
                     destinationDirectory.resolve(invoice.getRef() + fileExtension);

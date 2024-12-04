35431718d115155cf9458c645268e13f5cba5b9c
fanomezana <hei.fanomezana.2@gmail.com>
2024-12-04 13:06:44 +0300

    refactor: move BucketConf

--- src/main/java/app/bpartners/api/file/FileHash.java
+++
@@ -1,7 +0,0 @@
-package app.bpartners.api.file;
-
-import app.bpartners.api.PojaGenerated;
-
-@PojaGenerated
-@SuppressWarnings("all")
-public record FileHash(FileHashAlgorithm algorithm, String value) {}

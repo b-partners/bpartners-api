34e137f0e9b086cb924386bab2a7c9768f47b26d
nirinaHelena <hei.nirina.7@gmail.com>
2024-12-19 14:40:24 +0300

    test: rename generator to test

--- src/test/java/app/bpartners/api/unit/service/ReferenceGeneratorTest.java
+++ src/test/java/app/bpartners/api/unit/service/ReferenceGeneratorTest.java
@@ -1,7 +1,8 @@
-package app.bpartners.api.service.invoice;
+package app.bpartners.api.unit.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import app.bpartners.api.service.invoice.ReferenceGenerator;
 import java.time.LocalDateTime;
 import java.util.function.Supplier;
 import org.junit.jupiter.api.Test;
@@ -12,10 +13,11 @@ class ReferenceGeneratorTest {
   void testGet() {
     LocalDateTime fixedDateTime = LocalDateTime.of(2024, 12, 19, 15, 30, 45);
     Supplier<LocalDateTime> fixedDateTimeSupplier = () -> fixedDateTime;
-    ReferenceGenerator generator = new ReferenceGenerator(fixedDateTimeSupplier);
+    ReferenceGenerator subject = new ReferenceGenerator(fixedDateTimeSupplier);
+    var expected = "REF-19122024153045";
 
-    String reference = generator.get();
+    String actual = subject.get();
 
-    assertEquals("REF-19122024153045", reference);
+    assertEquals(expected, actual);
   }
 }

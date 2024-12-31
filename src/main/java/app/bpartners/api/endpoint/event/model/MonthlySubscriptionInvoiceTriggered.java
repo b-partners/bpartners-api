5140d3d93029b988099de3bea3dfb779d2586903
Ryan Andriamahery <ryan@hei.school>
2024-12-31 10:54:50 +0300

    chore: handle MonthlySubscriptionInvoice event with EVENT_STACK_2

--- src/main/java/app/bpartners/api/endpoint/event/model/MonthlySubscriptionInvoiceTriggered.java
+++ src/main/java/app/bpartners/api/endpoint/event/model/MonthlySubscriptionInvoiceTriggered.java
@@ -1,6 +1,9 @@
 package app.bpartners.api.endpoint.event.model;
 
+import static app.bpartners.api.endpoint.event.EventStack.EVENT_STACK_2;
+
 import java.time.Duration;
+import app.bpartners.api.endpoint.event.EventStack;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -19,5 +22,10 @@ public class MonthlySubscriptionInvoiceTriggered extends PojaEvent {
   @Override
   public Duration maxConsumerBackoffBetweenRetries() {
     return Duration.ofSeconds(60L);
+  }
+
+  @Override
+  public EventStack getEventStack() {
+    return EVENT_STACK_2;
   }
 }

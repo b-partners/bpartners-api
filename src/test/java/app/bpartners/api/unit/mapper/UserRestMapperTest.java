8a18c34f199fe713c1219b28c76a9eb2b48c2c03
nirinaHelena <hei.nirina.7@gmail.com>
2025-01-07 16:20:46 +0300

    test: UserRestMapperTest::user_to_rest_check_subscription_start_end_and_status

--- src/test/java/app/bpartners/api/unit/mapper/UserRestMapperTest.java
+++ src/test/java/app/bpartners/api/unit/mapper/UserRestMapperTest.java
@@ -1,10 +1,11 @@
 package app.bpartners.api.unit.mapper;
 
+import static app.bpartners.api.endpoint.rest.model.EnableStatus.ENABLED;
 import static app.bpartners.api.endpoint.rest.model.UserSubscriptionStatus.*;
 import static java.time.Instant.now;
+import static java.time.LocalTime.MAX;
 import static java.time.temporal.ChronoUnit.DAYS;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -14,10 +15,14 @@ import app.bpartners.api.endpoint.rest.mapper.UserRestMapper;
 import app.bpartners.api.model.User;
 import app.bpartners.api.model.subscription.Subscription;
 import app.bpartners.api.model.subscription.UserSubscription;
+import app.bpartners.api.model.subscription.UserSubscriptionEligible;
 import app.bpartners.api.repository.jpa.UserSubscriptionEligibleJpaRepository;
 import app.bpartners.api.service.subscription.SubscriptionService;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -33,6 +38,29 @@ class UserRestMapperTest {
   @BeforeEach
   void setUp() {
     when(subscriptionEligibleJpaRepositoryMock.findByUserId(any())).thenReturn(Optional.empty());
+  }
+
+  @Test
+  void user_to_rest_check_subscription_start_end_and_status() {
+    var domain = User.builder().status(ENABLED).roles(List.of()).build();
+    when(subscriptionServiceMock.getSubscriptionByUser(any()))
+        .thenReturn(UserSubscription.builder().build());
+    var subscriptionEligible =
+        UserSubscriptionEligible.builder().eligibleFrom(LocalDate.now()).build();
+    when(subscriptionEligibleJpaRepositoryMock.findByUserId(any()))
+        .thenReturn(Optional.ofNullable(subscriptionEligible));
+
+    var actual = subject.toRest(domain);
+
+    assertEquals(FREE_TRIAL, actual.getSubscriptionStatus());
+    var parisZoneId = ZoneId.of("Europe/Paris");
+    assertNotNull(subscriptionEligible);
+    var subscriptionEnd =
+        subscriptionEligible.getLatestTrialPeriodDate().atTime(MAX).atZone(parisZoneId).toInstant();
+    assertEquals(subscriptionEnd, Objects.requireNonNull(actual.getSubscription()).getEnd());
+    var subscriptionStart =
+        subscriptionEligible.getEligibleFrom().atStartOfDay(parisZoneId).toInstant();
+    assertEquals(subscriptionStart, actual.getSubscription().getStart());
   }
 
   @Test

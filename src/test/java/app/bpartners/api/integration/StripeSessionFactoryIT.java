48afba1a4cac735fd7c58abf0f1110dacfaa2a3c
nirinaHelena <hei.nirina.7@gmail.com>
2025-04-25 10:03:24 +0300

    refactor: delay charge to next month if trial ends between 1 to 4 of actual month or next month

--- src/test/java/app/bpartners/api/integration/StripeSessionFactoryIT.java
+++ src/test/java/app/bpartners/api/integration/StripeSessionFactoryIT.java
@@ -66,6 +66,8 @@ class StripeSessionFactoryIT extends StripeMockedThirdParties {
     var startOfNextMonth = LocalDate.of(2025, MAY, 1);
     var fourthOfNextMonth = LocalDate.of(2025, MAY, 4);
     var endOfActualMonth = LocalDate.of(2025, APRIL, 30);
+    var startOfActualMonth = LocalDate.of(2025, APRIL, 1);
+    var fourthOfActualMonth = LocalDate.of(2025, APRIL, 4);
     var billingCycleAnchor =
         Date.from(billingCycleAnchorLocalDate.atStartOfDay(ZoneId.of("Europe/Paris")).toInstant())
                 .getTime()
@@ -86,49 +88,8 @@ class StripeSessionFactoryIT extends StripeMockedThirdParties {
     when(temporalUtilsMock.startOfNextMonth()).thenReturn(startOfNextMonth);
     when(temporalUtilsMock.fourthOfNextMonth()).thenReturn(fourthOfNextMonth);
     when(temporalUtilsMock.endOfActualMonth()).thenReturn(endOfActualMonth);
-
-    var actual =
-        subject.createSession(
-            trialEnd,
-            stripeCustomer,
-            subscriptionProduct,
-            price,
-            redirectionUrls,
-            billingCycleAnchor,
-            subscription);
-
-    log.info("Session: {}", actual);
-    assertEquals("subscription", actual.getMode());
-    assertEquals("https://success.url", actual.getSuccessUrl());
-  }
-
-  @Test
-  void create_session_if_trial_end_is_end_of_actual_month() throws StripeException {
-    var trialEnd = LocalDate.of(2025, APRIL, 30);
-    var billingCycleAnchorLocalDate = LocalDate.of(2025, MAY, 5);
-    var startOfNextMonth = LocalDate.of(2025, MAY, 1);
-    var fourthOfNextMonth = LocalDate.of(2025, MAY, 4);
-    var endOfActualMonth = LocalDate.of(2025, APRIL, 30);
-    var billingCycleAnchor =
-        Date.from(billingCycleAnchorLocalDate.atStartOfDay(ZoneId.of("Europe/Paris")).toInstant())
-                .getTime()
-            / 1000L;
-    var stripeCustomer = mock(Customer.class);
-    var subscriptionProduct = mock(SubscriptionProduct.class);
-    var price = mock(Price.class);
-    var redirectionUrls = mock(RedirectionStatusUrls.class);
-    var subscription = mock(Subscription.class);
-    when(stripeCustomer.getId()).thenReturn(testCustomer.getId());
-    when(subscription.getSubscriptionProduct()).thenReturn(subscriptionProduct);
-    when(subscriptionProduct.getE2Id()).thenReturn(subscriptionProductRoofAnalysis.getE2Id());
-    when(subscriptionProduct.getPriceInCents()).thenReturn(1000L);
-    when(subscriptionProduct.getType()).thenReturn(MONTHLY);
-    when(price.getId()).thenReturn(testPrice.getId());
-    when(redirectionUrls.getSuccessUrl()).thenReturn("https://success.url");
-    when(redirectionUrls.getFailureUrl()).thenReturn("https://cancel.url");
-    when(temporalUtilsMock.startOfNextMonth()).thenReturn(startOfNextMonth);
-    when(temporalUtilsMock.fourthOfNextMonth()).thenReturn(fourthOfNextMonth);
-    when(temporalUtilsMock.endOfActualMonth()).thenReturn(endOfActualMonth);
+    when(temporalUtilsMock.startOfActualMonth()).thenReturn(startOfActualMonth);
+    when(temporalUtilsMock.fourthOfActualMonth()).thenReturn(fourthOfActualMonth);
 
     var actual =
         subject.createSession(
@@ -152,6 +113,8 @@ class StripeSessionFactoryIT extends StripeMockedThirdParties {
     var startOfNextMonth = LocalDate.of(2025, MAY, 1);
     var fourthOfNextMonth = LocalDate.of(2025, MAY, 4);
     var endOfActualMonth = LocalDate.of(2025, APRIL, 30);
+    var startOfActualMonth = LocalDate.of(2025, APRIL, 1);
+    var fourthOfActualMonth = LocalDate.of(2025, APRIL, 4);
     var billingCycleAnchor =
         Date.from(billingCycleAnchorLocalDate.atStartOfDay(ZoneId.of("Europe/Paris")).toInstant())
                 .getTime()
@@ -172,6 +135,55 @@ class StripeSessionFactoryIT extends StripeMockedThirdParties {
     when(temporalUtilsMock.startOfNextMonth()).thenReturn(startOfNextMonth);
     when(temporalUtilsMock.fourthOfNextMonth()).thenReturn(fourthOfNextMonth);
     when(temporalUtilsMock.endOfActualMonth()).thenReturn(endOfActualMonth);
+    when(temporalUtilsMock.startOfActualMonth()).thenReturn(startOfActualMonth);
+    when(temporalUtilsMock.fourthOfActualMonth()).thenReturn(fourthOfActualMonth);
+
+    var actual =
+        subject.createSession(
+            trialEnd,
+            stripeCustomer,
+            subscriptionProduct,
+            price,
+            redirectionUrls,
+            billingCycleAnchor,
+            subscription);
+
+    log.info("Session: {}", actual);
+    assertEquals("setup", actual.getMode());
+    assertEquals("https://success.url", actual.getSuccessUrl());
+  }
+
+  @Test
+  void create_session_if_trial_end_is_between_1_to_4_actual_month() throws StripeException {
+    var trialEnd = LocalDate.of(2025, APRIL, 1);
+    var billingCycleAnchorLocalDate = LocalDate.of(2025, MAY, 5);
+    var startOfNextMonth = LocalDate.of(2025, MAY, 1);
+    var fourthOfNextMonth = LocalDate.of(2025, MAY, 4);
+    var endOfActualMonth = LocalDate.of(2025, APRIL, 30);
+    var startOfActualMonth = LocalDate.of(2025, APRIL, 1);
+    var fourthOfActualMonth = LocalDate.of(2025, APRIL, 4);
+    var billingCycleAnchor =
+        Date.from(billingCycleAnchorLocalDate.atStartOfDay(ZoneId.of("Europe/Paris")).toInstant())
+                .getTime()
+            / 1000L;
+    var stripeCustomer = mock(Customer.class);
+    var subscriptionProduct = mock(SubscriptionProduct.class);
+    var price = mock(Price.class);
+    var redirectionUrls = mock(RedirectionStatusUrls.class);
+    var subscription = mock(Subscription.class);
+    when(stripeCustomer.getId()).thenReturn(testCustomer.getId());
+    when(subscription.getSubscriptionProduct()).thenReturn(subscriptionProduct);
+    when(subscriptionProduct.getE2Id()).thenReturn(subscriptionProductRoofAnalysis.getE2Id());
+    when(subscriptionProduct.getPriceInCents()).thenReturn(1000L);
+    when(subscriptionProduct.getType()).thenReturn(MONTHLY);
+    when(price.getId()).thenReturn(testPrice.getId());
+    when(redirectionUrls.getSuccessUrl()).thenReturn("https://success.url");
+    when(redirectionUrls.getFailureUrl()).thenReturn("https://cancel.url");
+    when(temporalUtilsMock.startOfNextMonth()).thenReturn(startOfNextMonth);
+    when(temporalUtilsMock.fourthOfNextMonth()).thenReturn(fourthOfNextMonth);
+    when(temporalUtilsMock.endOfActualMonth()).thenReturn(endOfActualMonth);
+    when(temporalUtilsMock.startOfActualMonth()).thenReturn(startOfActualMonth);
+    when(temporalUtilsMock.fourthOfActualMonth()).thenReturn(fourthOfActualMonth);
 
     var actual =
         subject.createSession(
@@ -195,6 +207,8 @@ class StripeSessionFactoryIT extends StripeMockedThirdParties {
     var startOfNextMonth = LocalDate.of(2025, MAY, 1);
     var fourthOfNextMonth = LocalDate.of(2025, MAY, 4);
     var endOfActualMonth = LocalDate.of(2025, APRIL, 30);
+    var startOfActualMonth = LocalDate.of(2025, APRIL, 1);
+    var fourthOfActualMonth = LocalDate.of(2025, APRIL, 4);
     var billingCycleAnchor =
         Date.from(billingCycleAnchorLocalDate.atStartOfDay(ZoneId.of("Europe/Paris")).toInstant())
                 .getTime()
@@ -215,6 +229,8 @@ class StripeSessionFactoryIT extends StripeMockedThirdParties {
     when(temporalUtilsMock.startOfNextMonth()).thenReturn(startOfNextMonth);
     when(temporalUtilsMock.fourthOfNextMonth()).thenReturn(fourthOfNextMonth);
     when(temporalUtilsMock.endOfActualMonth()).thenReturn(endOfActualMonth);
+    when(temporalUtilsMock.startOfActualMonth()).thenReturn(startOfActualMonth);
+    when(temporalUtilsMock.fourthOfActualMonth()).thenReturn(fourthOfActualMonth);
 
     var actual =
         subject.createSession(

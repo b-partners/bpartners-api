cce7115faa0d9bfb0d3a87f5cb3682a02ec11181
Daniel Langio <langio.tehiniavo@gmail.com>
2025-10-10 14:50:27 +0300

    chore: return a byte array on POST /accounts/{aId}/annotations/exports

--- src/test/java/app/bpartners/api/unit/service/AreaPictureAnnotationServiceTest.java
+++ src/test/java/app/bpartners/api/unit/service/AreaPictureAnnotationServiceTest.java
@@ -1,41 +1,32 @@
 package app.bpartners.api.unit.service;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
-import app.bpartners.api.endpoint.event.EventProducer;
-import app.bpartners.api.endpoint.event.model.ExportAreaPictureAnnotationRequested;
-import app.bpartners.api.endpoint.rest.model.ExportAreaPictureAnnotation;
+import app.bpartners.api.endpoint.rest.model.*;
 import app.bpartners.api.repository.AreaPictureAnnotationRepository;
+import app.bpartners.api.service.annotation.ExportAreaPictureAnnotationPDFProcessor;
 import app.bpartners.api.service.areapicture.AreaPictureAnnotationService;
-import java.util.List;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class AreaPictureAnnotationServiceTest {
-  EventProducer<ExportAreaPictureAnnotationRequested> eventProducerMock = mock();
+  ExportAreaPictureAnnotationPDFProcessor exportAreaPictureAnnotationPDFProcessorMock = mock();
   AreaPictureAnnotationService subject =
       new AreaPictureAnnotationService(
-          mock(AreaPictureAnnotationRepository.class), eventProducerMock);
+          mock(AreaPictureAnnotationRepository.class), exportAreaPictureAnnotationPDFProcessorMock);
 
   @Test
+  @SneakyThrows
   void export_area_picture_annotation_ok() {
-    var userId = "userId";
     var exportAreaPictureAnnotationMock = mock(ExportAreaPictureAnnotation.class);
-    var expected =
-        ExportAreaPictureAnnotationRequested.builder()
-            .annotation(exportAreaPictureAnnotationMock)
-            .userId(userId)
-            .build();
+    var expected = new byte[1];
 
-    doNothing().when(eventProducerMock).accept(any());
+    when(exportAreaPictureAnnotationPDFProcessorMock.process(any())).thenReturn(expected);
 
-    var actual = subject.exportAreaPictureAnnotationToPdf(userId, exportAreaPictureAnnotationMock);
+    var actual = subject.exportAreaPictureAnnotationToPdf(exportAreaPictureAnnotationMock);
 
-    assertEquals(expected.getAnnotation(), actual);
-    verify(eventProducerMock, times(1)).accept(List.of(expected));
+    Assertions.assertEquals(expected, actual);
   }
 }

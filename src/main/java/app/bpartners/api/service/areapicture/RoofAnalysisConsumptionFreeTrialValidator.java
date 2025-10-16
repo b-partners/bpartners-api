d0baac01ac1fa13b5a2cb3159aaf9775fee29add
Dinasoa <hei.dinasoa@gmail.com>
2025-10-16 10:33:43 +0300

    chore: exclude another user

--- src/main/java/app/bpartners/api/service/areapicture/RoofAnalysisConsumptionFreeTrialValidator.java
+++ src/main/java/app/bpartners/api/service/areapicture/RoofAnalysisConsumptionFreeTrialValidator.java
@@ -9,6 +9,7 @@ import app.bpartners.api.model.subscription.UserSubscriptionEligible;
 import app.bpartners.api.repository.jpa.UserApiKeyFullAuthorizationJpaRepository;
 import app.bpartners.api.service.subscription.SubscriptionService;
 import java.time.ZoneId;
+import java.util.List;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -20,13 +21,16 @@ public class RoofAnalysisConsumptionFreeTrialValidator
   private static final long DEFAULT_MAX_CONSUMPTION = 20L;
   private final SubscriptionService subscriptionService;
   private final UserApiKeyFullAuthorizationJpaRepository apiKeyFullAuthorizationRepository;
-  private String EXCLUDED_USER_ID="2ede5d19-fa49-4ad7-aa90-42c016a3f4f5";
+  private static final List<String> EXCLUDED_USER_IDS = List.of(
+          "6d394379-585e-4471-b42e-213dc7624a55",
+          "2ede5d19-fa49-4ad7-aa90-42c016a3f4f5"
+  );
 
   @Override
   public void accept(UserSubscriptionEligible userSubscriptionEligible) {
     var userId = userSubscriptionEligible.getUserId();
 
-    if (!userSubscriptionEligible.hasFreeTrialPeriodActive() || userId.equals(EXCLUDED_USER_ID)) {
+    if (!userSubscriptionEligible.hasFreeTrialPeriodActive() || EXCLUDED_USER_IDS.contains(userId)) {
       return;
     }
 

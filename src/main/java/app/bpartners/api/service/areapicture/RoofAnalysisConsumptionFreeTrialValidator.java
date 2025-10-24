072efe29cc343ead77b4f6cf32ef39d0265a99fd
RickaPrincy <rckprincy@gmail.com>
2025-10-24 21:49:24 +0300

    style: format

--- src/main/java/app/bpartners/api/service/areapicture/RoofAnalysisConsumptionFreeTrialValidator.java
+++ src/main/java/app/bpartners/api/service/areapicture/RoofAnalysisConsumptionFreeTrialValidator.java
@@ -21,16 +21,15 @@ public class RoofAnalysisConsumptionFreeTrialValidator
   private static final long DEFAULT_MAX_CONSUMPTION = 20L;
   private final SubscriptionService subscriptionService;
   private final UserApiKeyFullAuthorizationJpaRepository apiKeyFullAuthorizationRepository;
-  private static final List<String> EXCLUDED_USER_IDS = List.of(
-          "6d394379-585e-4471-b42e-213dc7624a55",
-          "2ede5d19-fa49-4ad7-aa90-42c016a3f4f5"
-  );
+  private static final List<String> EXCLUDED_USER_IDS =
+      List.of("6d394379-585e-4471-b42e-213dc7624a55", "2ede5d19-fa49-4ad7-aa90-42c016a3f4f5");
 
   @Override
   public void accept(UserSubscriptionEligible userSubscriptionEligible) {
     var userId = userSubscriptionEligible.getUserId();
 
-    if (!userSubscriptionEligible.hasFreeTrialPeriodActive() || EXCLUDED_USER_IDS.contains(userId)) {
+    if (!userSubscriptionEligible.hasFreeTrialPeriodActive()
+        || EXCLUDED_USER_IDS.contains(userId)) {
       return;
     }
 
@@ -49,7 +48,7 @@ public class RoofAnalysisConsumptionFreeTrialValidator
             .orElse(0L);
 
     if (actualRoofAnalysisConsumption >= DEFAULT_MAX_CONSUMPTION
-        &&  apiKeyFullAuthorizationRepository.findByIdUser(userId).isEmpty()) {
+        && apiKeyFullAuthorizationRepository.findByIdUser(userId).isEmpty()) {
       throw new BadRequestException(
           "Roof analysis consumption "
               + actualRoofAnalysisConsumption

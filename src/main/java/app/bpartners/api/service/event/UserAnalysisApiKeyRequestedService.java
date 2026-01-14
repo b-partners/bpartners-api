8966cacbb787ff7ee264775285c66f0163142da8
Daniel Langio <langio.tehiniavo@gmail.com>
2026-01-14 11:24:10 +0300

    fix(UserAnalysisApiKeyRequestedService): respect spring dependency inversion for UserRepository dependency

--- src/main/java/app/bpartners/api/service/event/UserAnalysisApiKeyRequestedService.java
+++ src/main/java/app/bpartners/api/service/event/UserAnalysisApiKeyRequestedService.java
@@ -3,7 +3,7 @@ package app.bpartners.api.service.event;
 import app.bpartners.api.endpoint.event.model.UserAnalysisApiKeyRequested;
 import app.bpartners.api.model.User;
 import app.bpartners.api.model.UserAnalysisApiKey;
-import app.bpartners.api.repository.implementation.UserRepositoryImpl;
+import app.bpartners.api.repository.UserRepository;
 import app.bpartners.api.service.user.UserAnalyseApiKeyService;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserAnalysisApiKeyRequestedService implements Consumer<UserAnalysisApiKeyRequested> {
-  private final UserRepositoryImpl userRepository;
+  private final UserRepository userRepository;
   private final UserAnalyseApiKeyService service;
 
   @Override

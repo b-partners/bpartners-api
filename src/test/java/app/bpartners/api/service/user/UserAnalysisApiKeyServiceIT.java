9768133b15f75f6e00310ba039f9368319d1ccbf
Daniel Langio <langio.tehiniavo@gmail.com>
2026-01-15 16:22:48 +0300

    chore(UserAnalysisApiKeyServiceIT): disable for local use only

--- src/test/java/app/bpartners/api/service/user/UserAnalysisApiKeyServiceIT.java
+++ src/test/java/app/bpartners/api/service/user/UserAnalysisApiKeyServiceIT.java
@@ -1,20 +1,23 @@
-package app.bpartners.api.service.event;
+package app.bpartners.api.service.user;
 
 import static java.util.UUID.randomUUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 import app.bpartners.api.model.User;
-import app.bpartners.api.service.user.UserAnalysisApiKeyService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.client.RestTemplate;
 
 @Slf4j
+@Disabled("Local use only")
 class UserAnalysisApiKeyServiceIT {
   private static final String GEO_JOBS_API_KEY = "<todo>";
   private static final String GEO_JOBS_BASE_URL = "https://api.birdia.fr";
   private static final String USER_ID = randomUUID().toString();
-  private static final String EMAIL = "joe@tester.com"; // create a user with this email before
+  private static final String USER_EMAIL = "joe@tester.com"; // create a user with this email before
+  private static final String USER_FIRST_NAME = "Joe";
+  private static final String USER_LAST_NAME = "Tester";
 
   RestTemplate restTemplate = new RestTemplate();
 
@@ -22,7 +25,7 @@ class UserAnalysisApiKeyServiceIT {
       new UserAnalysisApiKeyService(GEO_JOBS_BASE_URL, GEO_JOBS_API_KEY, restTemplate);
 
   @Test
-  void accept_ok() {
+  void get_analysis_api_key_ok() {
 
     var actual = subject.getAnalysisApiKey(user());
 
@@ -31,6 +34,11 @@ class UserAnalysisApiKeyServiceIT {
   }
 
   private User user() {
-    return User.builder().id(USER_ID).firstName("Joe").lastName("Tester").email(EMAIL).build();
+    return User.builder()
+        .id(USER_ID)
+        .firstName(USER_FIRST_NAME)
+        .lastName(USER_LAST_NAME)
+        .email(USER_EMAIL)
+        .build();
   }
 }

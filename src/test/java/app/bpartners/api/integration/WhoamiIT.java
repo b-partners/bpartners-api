3d47e7f2c3e1feac9ff1c7b921de8cb9f26c5c71
Daniel Langio <langio.tehiniavo@gmail.com>
2026-01-28 16:37:53 +0300

    chore(WhoamiIT): disable whoami_with_api_key

--- src/test/java/app/bpartners/api/integration/WhoamiIT.java
+++ src/test/java/app/bpartners/api/integration/WhoamiIT.java
@@ -24,6 +24,7 @@ import java.util.List;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -68,6 +69,7 @@ class WhoamiIT extends MockedThirdParties {
     assertEquals(restJoeDoeUser().id(actual.getUser().getId()), actual.getUser().roles(List.of()));
   }
 
+  @Disabled("flaky")
   @Test
   void whoami_with_api_key() throws ApiException {
     ApiClient client = anApiClientWithApiKey();

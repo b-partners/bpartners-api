c71b1e1bfb90ab4f2f593bc8ff3ca0d653db66c2
Daniel Langio <langio.tehiniavo@gmail.com>
2026-01-30 10:56:21 +0300

    chore: mark analysis key revocation as not implemented

--- src/main/java/app/bpartners/api/service/user/ApiKeyService.java
+++ src/main/java/app/bpartners/api/service/user/ApiKeyService.java
@@ -1,6 +1,5 @@
 package app.bpartners.api.service.user;
 
-import static app.bpartners.api.endpoint.rest.model.UserApiKeyType.ANALYSIS;
 import static app.bpartners.api.endpoint.rest.model.UserApiKeyType.DASHBOARD;
 import static app.bpartners.api.endpoint.rest.security.model.Role.ADMIN_ROLE;
 
@@ -9,6 +8,7 @@ import app.bpartners.api.model.User;
 import app.bpartners.api.model.UserAnalysisApiKey;
 import app.bpartners.api.model.exception.BadRequestException;
 import app.bpartners.api.model.exception.ForbiddenException;
+import app.bpartners.api.model.exception.NotImplementedException;
 import app.bpartners.api.repository.UserAnalysisApiKeyRepository;
 import jakarta.transaction.Transactional;
 import java.util.List;
@@ -33,29 +33,27 @@ public class ApiKeyService {
   private UserApiKey revokeApiKey(String key, User user) {
     UserAnalysisApiKey userAnalysisApiKey = userAnalysisApiKeyRepository.getByApiKey(key);
     if (userAnalysisApiKey != null) {
-      User apiKeyOwner = userAnalysisApiKey.getUser();
-      if (!Objects.equals(apiKeyOwner.getId(), user.getId())
-          && !user.getRoles().contains(ADMIN_ROLE)) {
-        throw new ForbiddenException("Users can only revoke it's own api key");
-      }
-
-      UserAnalysisApiKey revokedAnalysisApiKey =
-          userAnalysisApiKeyRepository.save(userAnalysisApiKey.toBuilder().enabled(false).build());
-      return new UserApiKey()
-          .key(revokedAnalysisApiKey.getApiKey())
-          .enabled(revokedAnalysisApiKey.isEnabled())
-          .type(ANALYSIS)
-          .expirationDatetime(revokedAnalysisApiKey.getExpirationDatetime())
-          .creationDatetime(revokedAnalysisApiKey.getCreationDatetime());
+      return revokeAnalysisApiKey(key, user);
     }
+
+    return revokeDashboardApiKey(key, user);
+  }
+
+  private UserApiKey revokeAnalysisApiKey(String key, User user) {
+    throw new NotImplementedException("Revoking analysis api key is not supported yet");
+  }
+
+  private UserApiKey revokeDashboardApiKey(String key, User user) {
     var userApiKeyOwner = userService.getUserByApiKey(key);
-    if (userApiKeyOwner == null && !user.getRoles().contains(ADMIN_ROLE)) {
+
+    if (userApiKeyOwner == null) {
       throw new BadRequestException("No users found with api key " + key);
     }
     if (!Objects.equals(userApiKeyOwner.getId(), user.getId())
         && !user.getRoles().contains(ADMIN_ROLE)) {
       throw new ForbiddenException("Users can only revoke it's own api key");
     }
+
     var savedUserWithRevokedApiKey =
         userService.save(userApiKeyOwner.toBuilder().apiKey(null).build());
     return new UserApiKey()

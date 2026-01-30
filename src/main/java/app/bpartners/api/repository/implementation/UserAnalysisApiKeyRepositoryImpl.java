a20aac7d2c53702b3f1b734db8864d9e94ceba26
Daniel Langio <langio.tehiniavo@gmail.com>
2026-01-31 01:41:19 +0300

    fix: handle multiple analysis api key found by one key

--- src/main/java/app/bpartners/api/repository/implementation/UserAnalysisApiKeyRepositoryImpl.java
+++ src/main/java/app/bpartners/api/repository/implementation/UserAnalysisApiKeyRepositoryImpl.java
@@ -7,12 +7,15 @@ import app.bpartners.api.repository.UserAnalysisApiKeyRepository;
 import app.bpartners.api.repository.UserRepository;
 import app.bpartners.api.repository.jpa.UserAnalysisApiKeyJpaRepository;
 import app.bpartners.api.repository.jpa.model.HUserAnalysisApiKey;
+import java.util.HashSet;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 @AllArgsConstructor
 @Repository
+@Slf4j
 public class UserAnalysisApiKeyRepositoryImpl implements UserAnalysisApiKeyRepository {
   private final UserAnalysisApiKeyJpaRepository jpaRepository;
   private final UserRepository userRepository;
@@ -28,7 +31,17 @@ public class UserAnalysisApiKeyRepositoryImpl implements UserAnalysisApiKeyRepos
 
   @Override
   public UserAnalysisApiKey getByApiKey(String apiKey) {
-    HUserAnalysisApiKey hApiKey = jpaRepository.getByApiKey(apiKey);
+    List<HUserAnalysisApiKey> hApiKeys = jpaRepository.getByApiKey(apiKey);
+
+    if (hApiKeys.size() > 1) {
+      List<String> ownersIds = hApiKeys.stream().map(HUserAnalysisApiKey::getUserId).toList();
+      log.warn(
+          "Multiple analysis belonging to users {} api key have the same key : {}",
+          String.join(", ", new HashSet<>(ownersIds)),
+          hApiKeys.getFirst().getApiKey());
+    }
+
+    HUserAnalysisApiKey hApiKey = hApiKeys.stream().findFirst().orElse(null);
     if (hApiKey == null) {
       return null;
     }

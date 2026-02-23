580f368ad7ca94f192fe61ede4e6811a8ce199b2
Dinasoa <hei.dinasoa@gmail.com>
2026-02-23 22:17:04 +0300

    feat: use image validator api to verify image

--- src/main/java/app/bpartners/api/service/wms/imageSource/ImageValidator.java
+++ src/main/java/app/bpartners/api/service/wms/imageSource/ImageValidator.java
@@ -1,87 +1,58 @@
 package app.bpartners.api.service.wms.imageSource;
 
-import static app.bpartners.api.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-import app.bpartners.api.model.exception.ApiException;
 import app.bpartners.api.service.wms.imageSource.exception.BlankImageException;
-import java.awt.Color;
-import java.awt.image.BufferedImage;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
-import javax.imageio.ImageIO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 @Slf4j
 public class ImageValidator implements Consumer<File> {
+  private static final String IMAGE_VALIDATOR_API_URL = System.getenv("IMAGE_VALIDATOR_API_URL");
+  private final RestTemplate restTemplate;
+  private final ObjectMapper om = new ObjectMapper();
+
+  public ImageValidator(RestTemplate restTemplate) {
+    this.restTemplate = restTemplate;
+  }
+
+  @Override
   public void accept(File file) throws BlankImageException {
-    log.info("File content: {}", file);
-
-    if (file == null) {
-      throw new ApiException(SERVER_EXCEPTION, "File is null");
-    }
-
     try {
-      BufferedImage image = ImageIO.read(file);
-      if (image == null) {
-        throw new ApiException(SERVER_EXCEPTION, "Image is null");
-      }
+      byte[] fileBytes = Files.readAllBytes(file.toPath());
+      String base64 = Base64.getEncoder().encodeToString(fileBytes);
+      Map<String, String> body = new HashMap<>();
+      body.put("base64image", base64);
+      HttpHeaders headers = new HttpHeaders();
+      headers.setContentType(APPLICATION_JSON);
+      HttpEntity<Map<String, String>> request = new HttpEntity<>(body, headers);
 
-      int w = image.getWidth();
-      int h = image.getHeight();
-      int[] pixels = image.getRGB(0, 0, w, h, null, 0, w);
+      ResponseEntity<String> response =
+          restTemplate.postForEntity(IMAGE_VALIDATOR_API_URL, request, String.class);
 
-      boolean isBlank = true;
-      boolean isBlack = true;
-      boolean hasRed = false;
+      JsonNode jsonNode = om.readTree(response.getBody());
+      boolean isCorrupted = jsonNode.get("isCorrupted").asBoolean();
 
-      for (int pixel : pixels) {
-
-        if ((pixel & 0xFF000000) != 0 && !isWhite(pixel)) {
-          isBlank = false;
-        }
-
-        if (pixel != Color.BLACK.getRGB()) {
-          isBlack = false;
-        }
-
-        if (isRed(pixel)) {
-          hasRed = true;
-        }
-
-        if (!isBlank && !isBlack && hasRed) {
-          break;
-        }
-      }
-
-      if (isBlank) {
-        throw new BlankImageException("Image from " + file.getName() + " is blank");
-      }
-
-      if (isBlack) {
-        throw new BlankImageException("Image from " + file.getName() + " is completely black");
-      }
-
-      if (hasRed) {
-        throw new BlankImageException("Image from " + file.getName() + " contains red pixels");
+      if (isCorrupted) {
+        throw new BlankImageException("Image is corrupted.");
       }
 
     } catch (IOException e) {
-      throw new ApiException(SERVER_EXCEPTION, e);
+      throw new RuntimeException("Error reading file", e);
     }
-  }
-
-  private boolean isRed(int pixel) {
-    int r = (pixel >> 16) & 0xFF;
-    int g = (pixel >> 8) & 0xFF;
-    int b = pixel & 0xFF;
-
-    return r > 150 && g < 100 && b < 100;
-  }
-
-  private boolean isWhite(int pixel) {
-    return (pixel & 0xFFFFFF) == 0xFFFFFF;
   }
 }

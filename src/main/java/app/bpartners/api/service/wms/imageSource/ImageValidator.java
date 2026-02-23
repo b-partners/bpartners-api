f60f59c2202b637b2dea75c33c44f4cdbae1d19f
Dinasoa <hei.dinasoa@gmail.com>
2026-02-23 18:20:43 +0300

    fix: throw exception if ign failed wmts is returned

--- src/main/java/app/bpartners/api/service/wms/imageSource/ImageValidator.java
+++ src/main/java/app/bpartners/api/service/wms/imageSource/ImageValidator.java
@@ -18,28 +18,40 @@ import org.springframework.stereotype.Component;
 public class ImageValidator implements Consumer<File> {
   public void accept(File file) throws BlankImageException {
     log.info("File content: {}", file);
+
     if (file == null) {
       throw new ApiException(SERVER_EXCEPTION, "File is null");
     }
+
     try {
       BufferedImage image = ImageIO.read(file);
       if (image == null) {
         throw new ApiException(SERVER_EXCEPTION, "Image is null");
       }
+
       int w = image.getWidth();
       int h = image.getHeight();
       int[] pixels = image.getRGB(0, 0, w, h, null, 0, w);
+
       boolean isBlank = true;
       boolean isBlack = true;
+      boolean hasRed = false;
 
       for (int pixel : pixels) {
+
         if ((pixel & 0xFF000000) != 0 && !isWhite(pixel)) {
           isBlank = false;
         }
+
         if (pixel != Color.BLACK.getRGB()) {
           isBlack = false;
         }
-        if (!isBlank && !isBlack) {
+
+        if (isRed(pixel)) {
+          hasRed = true;
+        }
+
+        if (!isBlank && !isBlack && hasRed) {
           break;
         }
       }
@@ -47,12 +59,26 @@ public class ImageValidator implements Consumer<File> {
       if (isBlank) {
         throw new BlankImageException("Image from " + file.getName() + " is blank");
       }
+
       if (isBlack) {
         throw new BlankImageException("Image from " + file.getName() + " is completely black");
       }
+
+      if (hasRed) {
+        throw new BlankImageException("Image from " + file.getName() + " contains red pixels");
+      }
+
     } catch (IOException e) {
       throw new ApiException(SERVER_EXCEPTION, e);
     }
+  }
+
+  private boolean isRed(int pixel) {
+    int r = (pixel >> 16) & 0xFF;
+    int g = (pixel >> 8) & 0xFF;
+    int b = pixel & 0xFF;
+
+    return r > 150 && g < 100 && b < 100;
   }
 
   private boolean isWhite(int pixel) {

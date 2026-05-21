c0a0242af7242dd58377542a4814d71d082d1314
Ryan Andriamahery <ryan@hei.school>
2026-05-21 14:28:47 +0300

    chore: reformat code

--- src/main/java/app/bpartners/api/repository/jpa/UnknownStripeCustomerJpaRepository.java
+++ src/main/java/app/bpartners/api/repository/jpa/UnknownStripeCustomerJpaRepository.java
@@ -1,9 +1,7 @@
 package app.bpartners.api.repository.jpa;
 
 import app.bpartners.api.model.UnknownStripeCustomer;
-
 import java.time.Instant;
-import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;

8bc54141f724e1de219ef1a095d83fefebb6ca9e
Daniel Langio <langio.tehiniavo@gmail.com>
2026-06-03 16:54:02 +0300

    format

--- src/test/java/app/bpartners/api/service/annotation/factory/ExportAnnotationContextFactoryTest.java
+++ src/test/java/app/bpartners/api/service/annotation/factory/ExportAnnotationContextFactoryTest.java
@@ -1,7 +1,6 @@
 package app.bpartners.api.service.annotation.factory;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 

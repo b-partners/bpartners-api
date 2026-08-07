655aadd5158873ee57b09a3ff560be407f5f9896
Ryan Andriamahery <ryan@hei.school>
2026-08-07 10:04:24 +0300

    feat: retrieve user subscription commitments

--- src/main/java/app/bpartners/api/repository/UserSubscriptionCommitmentJpaRepository.java
+++ src/main/java/app/bpartners/api/repository/UserSubscriptionCommitmentJpaRepository.java
@@ -1,7 +1,11 @@
 package app.bpartners.api.repository;
 
 import app.bpartners.api.model.UserSubscriptionCommitment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserSubscriptionCommitmentJpaRepository
-    extends JpaRepository<UserSubscriptionCommitment, String> {}
+    extends JpaRepository<UserSubscriptionCommitment, String> {
+
+  List<UserSubscriptionCommitment> findAllByUserId(String userId);
+}

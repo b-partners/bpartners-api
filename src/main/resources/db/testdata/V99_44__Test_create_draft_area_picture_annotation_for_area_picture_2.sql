ca7e548f01193771f1780946d3af527315c5d2d3
Daniel Langio <langio.tehiniavo@gmail.com>
2026-09-01 22:23:35 +0300

    fix: cap concurrent GeoData Imagery API calls in draft-annotations batching

---
+++ src/main/resources/db/testdata/V99_44__Test_create_draft_area_picture_annotation_for_area_picture_2.sql
@@ -0,0 +1,2 @@
+insert into "area_picture_annotation" (id, creation_datetime, id_user, id_area_picture, is_draft)
+values ('area_picture_annotation_6_id', '2024-01-08T01:02:30.00Z', 'joe_doe_id', 'area_picture_2_id', true);
